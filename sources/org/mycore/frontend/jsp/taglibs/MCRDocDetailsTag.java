c73c08d233ba4acba391b456f1793317bf7ae8b2
Heiko Helmbrecht <mcrhhelm@duepub-1.zim.uni-duisburg-essen.de>
2005-11-28 15:31:40 +0000

    add the attribute var, now you can nest the mcr:docDetails Tag
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@6222 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/jsp/taglibs/MCRDocDetailsTag.java
+++ sources/org/mycore/frontend/jsp/taglibs/MCRDocDetailsTag.java
@@ -9,12 +9,14 @@ import javax.servlet.jsp.*;
 import org.apache.log4j.Logger;
 import org.jdom.JDOMException;
 import org.jdom.output.DOMOutputter;
+import org.mycore.common.JSPUtils;
 import org.mycore.frontend.jsp.query.MCRResultFormatter;
 
 
 public class MCRDocDetailsTag extends SimpleTagSupport
 {
 	private org.jdom.Document mcrObj;
+	private String var;
 	private String lang;
 	
 	public void setMcrObj(org.jdom.Document inputDoc) {
@@ -25,6 +27,10 @@ public class MCRDocDetailsTag extends SimpleTagSupport
 		lang = inputLang;
 		return;
 	}
+	public void setVar(String inputVar) {
+		var = inputVar;
+		return;
+	}	
 	public void doTag() throws JspException, IOException {
 		org.jdom.Document allMetaValues = MCRResultFormatter.getFormattedDocDetails(mcrObj,lang);
 		org.w3c.dom.Document domDoc = null;
@@ -33,10 +39,19 @@ public class MCRDocDetailsTag extends SimpleTagSupport
 		} catch (JDOMException e) {
 			Logger.getLogger(MCRDocDetailsTag.class).error("Domoutput failed: ", e);
 		}
-		PageContext pageContext = (PageContext) getJspContext();
-		pageContext.setAttribute("docDetails", domDoc);
+		PageContext pageContext = (PageContext) getJspContext();		
+		pageContext.setAttribute(var, domDoc);
         JspFragment body = getJspBody();
         JspWriter out = pageContext.getOut();
+		if(pageContext.getAttribute("debug") != null && pageContext.getAttribute("debug").equals("true")) {
+			StringBuffer debugSB = new StringBuffer("<textarea cols=\"80\" rows=\"30\">")
+				.append("MCROBJ:\r\n").append(JSPUtils.getPrettyString(mcrObj))
+				.append("\r\n--------------------\r\n")
+				.append("formatted Content\r\n")
+				.append(JSPUtils.getPrettyString(allMetaValues))
+				.append("</textarea>");
+			out.println(debugSB.toString());
+		}        
         try {
             StringWriter stringWriter = new StringWriter();
             body.invoke(stringWriter);
