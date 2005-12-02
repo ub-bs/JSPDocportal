6a46251fd60d5d90bf7b5b7e39b72d4846c9522b
Heiko Helmbrecht <mcrhhelm@duepub-1.zim.uni-duisburg-essen.de>
2005-12-02 23:40:20 +0000

    eliminated naventry and navnode, navigation.xml is enough
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@6292 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/jsp/NavServlet.java
+++ sources/org/mycore/frontend/jsp/NavServlet.java
@@ -1,204 +1,212 @@
 package org.mycore.frontend.jsp;
 
-import org.jdom.Document;
 import org.jdom.Element;
-import org.jdom.input.SAXBuilder;
+import org.jdom.filter.ElementFilter;
 import org.jdom.xpath.XPath;
-import org.mycore.frontend.jsp.navigation.NavEntry;
-import org.mycore.frontend.jsp.navigation.NavNode;
-import org.mycore.frontend.jsp.navigation.NavTree;
 import org.mycore.frontend.servlets.MCRServlet;
+import org.mycore.frontend.servlets.MCRServletJob;
+import org.mycore.common.JSPUtils;
 import org.mycore.common.MCRSession;
+import org.mycore.common.xml.MCRURIResolver;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
 import javax.servlet.ServletContext;
-import javax.servlet.RequestDispatcher;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-
-
 import org.apache.log4j.Logger;
 
-public class NavServlet extends HttpServlet
+public class NavServlet extends MCRServlet
 {
-    private static Logger log = Logger.getLogger(NavServlet.class);
-    private static NavTree navTree;
-    private static String baseURL ;
+    private static Logger logger = Logger.getLogger(NavServlet.class);
 
-    public static void initialize()
+    private static String baseURL ;
+    private static org.jdom.Document navJdom;
+    private static org.w3c.dom.Document navDom;
+    
+    public void initializeParameters(HttpServletRequest request)
     {
-        getNewTree(); //precache
+        //getNewTree(); //precache
+    	setBaseURL(request);
+    	setNavJdom();
+    	setNavDom(navJdom);
     }
     
     public static void deinitialize()
     {
-        navTree = null; //free memory
+        //navTree = null; //free memory
         baseURL = null;
+        navJdom = null;
+        navDom = null;
     }    
     
-    private static NavTree getTree(boolean force) {
-    	if (!force && navTree != null) {
-    		return navTree;
-    	} else {
-    		return getNewTree();
-    	}
+    public void setBaseURL(HttpServletRequest request) {
+		logger.debug("baseURL is null");
+		ServletContext context = getServletContext();
+		String contextPath = request.getContextPath();
+		if (contextPath == null)
+			contextPath = "";
+		contextPath += "/";
+		String requestURL = request.getRequestURL().toString();
+		int pos = requestURL.indexOf(contextPath, 9);
+		baseURL = requestURL.substring(0, pos) + contextPath;
+    	if (context.getAttribute("WebApplicationBaseURL") == null) {
+    		context.setAttribute("WebApplicationBaseURL", baseURL);
+    	}    		
+		logger.debug("baseURL = " + baseURL);    	
+    }
+    public void setNavJdom() {
+    	ServletContext context = getServletContext();
+    	org.jdom.Element navigationEl = MCRURIResolver.instance().resolve("resource:navigation.xml");
+    	int i = 0;
+    	for (Iterator it = navigationEl.getDescendants(new ElementFilter()); it.hasNext();) {
+			Element navEl = (Element) it.next();
+			String path;
+			String extern = navEl.getAttributeValue("extern");
+			if(extern != null && extern.equals("true")) {
+				if(extern.indexOf(".") > -1)
+					path = navEl.getAttributeValue("href");
+				else
+					path = baseURL + navEl.getAttributeValue("href");
+			}else {
+				Element parent = navEl;
+				int k = 0;
+				StringBuffer pathSB = new StringBuffer("");
+				while(parent != null) {
+		        	if (!parent.getName().equals("navitem")) break;
+		        	if (k != 0) {
+		        		pathSB.insert(0,".");
+		        	}
+		        	pathSB.insert(0, parent.getAttributeValue("name"));
+		        	parent =  parent.getParentElement();
+		        	k++;					
+				}
+				pathSB.insert(0,"nav?path=").insert(0,baseURL);
+				path = pathSB.toString();
+			}
+			if(navEl.getName().equals("navitem")) {
+				navEl.setAttribute("path", path);
+			}
+			String systemID = JSPUtils.fillToConstantLength(String.valueOf(i),"0",4);
+			navEl.setAttribute("systemID",systemID);
+			i++;
+		}
+    	navJdom = navigationEl.getDocument();
+    	context.setAttribute("navJdom",navJdom);
+    	logger.debug(JSPUtils.getPrettyString(navJdom));
+    	return;
+    }
+    
+    public void setNavDom(org.jdom.Document jdom) {
+    	ServletContext context = getServletContext();
+    	org.w3c.dom.Document domDoc = null;
+		try {
+			domDoc = new org.jdom.output.DOMOutputter().output(jdom);
+		} catch (org.jdom.JDOMException e) {
+			Logger.getLogger(NavServlet.class).error("Domoutput failed: ", e);
+		}  
+		navDom = domDoc;
+		context.setAttribute("navDom",navDom);
+		return;
     }
     
     public static String getNavigationBaseURL() {
     	return baseURL ;
+
     }
     
-    private static NavTree getNewTree() 
+    protected void doGetPost(MCRServletJob job) throws Exception
     {
-    	
-        Document document;
-        NavTree tree = new NavTree();
-        
-        
-        try {
-        	InputStream in = Class.forName("org.mycore.frontend.jsp.NavServlet").getResourceAsStream("/navigation.xml");
-        	if (in == null) {
-                log.error("Missing file navigation.xml");
-                return null;
-            }            	
-
-        	SAXBuilder builder = new SAXBuilder(false);
-            document = builder.build(in);
-
-            XPath navigationXP = XPath.newInstance("/navigations/navigation");
-			XPath navitemXP = XPath.newInstance("navitem");
-			XPath refitemXP = XPath.newInstance("refitem");            
-            
-            for (Iterator navIT = navigationXP.selectNodes(document).iterator();
-            		navIT.hasNext();) {
-				Element navigation = (Element) navIT.next();
-
-				LinkedList lElements = new LinkedList();
-				LinkedList lPaths = new LinkedList();
-				
-				// Stack-Initialization
-				for (Iterator it = navitemXP.selectNodes(navigation).iterator(); it.hasNext();) {
-					Element navitem = (Element) it.next();
-					lElements.addLast(navitem);
-					lPaths.addLast(navitem.getAttributeValue("name"));
-				}
-				while(!lElements.isEmpty() && !lPaths.isEmpty()) {
-					Element navitem = (Element) lElements.removeFirst();
-					String path = (String) lPaths.removeFirst();
-					String description = navitem.getAttributeValue("label");
-					String page = navitem.getAttributeValue("href");
-					if ( (navitem.getAttributeValue("hidden") != null) && 
-						 (navitem.getAttributeValue("hidden").equals("true"))	) {
-						tree.addHiddenNode(path,description,page);
-					} else if ( (navitem.getAttributeValue("extern") != null) && 
-							    (navitem.getAttributeValue("extern").equals("true")) ) {
-						tree.addExternalNode(path,description,page);
-					} else {
-						tree.addNode(path,description,page);
-					}
-					for (Iterator it = navitemXP.selectNodes(navitem).iterator(); it.hasNext();) {
-						Element childNavitem = (Element) it.next();
-						lElements.addLast(childNavitem);
-						lPaths.addLast(path + "." + childNavitem.getAttributeValue("name"));
-					}
-					for (Iterator it = refitemXP.selectNodes(navitem).iterator(); it.hasNext();) {
-						Element refitem = (Element) it.next();
-						String refPath = refitem.getAttributeValue("name");
-						String refDescription = refitem.getAttributeValue("label");
-						String refLink = path;
-						tree.addReference(refPath,refDescription,refLink);
-					}					
-				}
-				 
-			}
-            navTree = tree;
-            
-        } catch (Exception e) {
-            log.error(e);
-        }
-    	return tree;
-    }    	
-    	
-
-
-    
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-    	if ((baseURL == null) || baseURL.equals(""))  {
-    		log.debug("baseURL is null");
-    		String contextPath = request.getContextPath();
-    		if (contextPath == null)
-    			contextPath = "";
-    		contextPath += "/";
-    		String requestURL = request.getRequestURL().toString();
-    		int pos = requestURL.indexOf(contextPath, 9);
-    		baseURL = requestURL.substring(0, pos) + contextPath;
-    		log.debug("baseURL = " + baseURL);
+    	HttpServletRequest request = job.getRequest();
+    	HttpServletResponse response = job.getResponse();
+    	ServletContext context = this.getServletContext();
+    	if ((baseURL == null) || baseURL.equals("") || (navJdom == null) || (navDom == null))  {
+    		initializeParameters(request);
     	}
-    	
         MCRSession session = MCRServlet.getSession(request);
-
-        ServletContext context = this.getServletContext();
         if(!"yes".equals(context.getAttribute("startup_done"))) {
             PrintWriter out = response.getWriter();
             out.write("<html><body><h1>Application offline</h1>The application is curerntly offline. Probably it's still starting up, or has been shut down.</body></html>");
             return;
         }
-        
-        NavTree tree = getTree(false);
+
+        org.jdom.Element youAreHere = new org.jdom.Element("youAreHere");
+        StringBuffer pathID = new StringBuffer("");
+        String nodeID = "";
         String path = request.getParameter("path");
-        if(path==null) {
+        if(path == null) {
             path = "left";
         }
+        logger.debug("Navigation servlet called with path "+path+" (session "+session.getID()+")");
 
-	log.debug("Navigation servlet called with path "+path+" (session "+session.getID()+")");
-
-        NavNode node;
-        NavEntry e;
-        while(true) {
-            node = tree.getChild(path);
-            e = node.getValue();
-            if(e.isReference()) {
-                path = e.getLink();
-            } else break;
+        Element navitem = null;
+        StringBuffer pathSB = new StringBuffer("");
+        if(path.startsWith("~")) {
+        	org.jdom.Element refitem = (Element) XPath.selectSingleNode(navJdom,"/navigations//refitem[@name='" + path + "']");
+            navitem = refitem.getParentElement();
+        }else{
+        	String[] nodes = path.split("\\.");
+        	StringBuffer xpath = new StringBuffer("/navigations/");
+        	for (int i = 0; i < nodes.length; i++) {
+        		logger.debug("i = " + i);
+        		xpath.append("/navitem[@name='").append(nodes[i]).append("']");
+			}
+        	navitem = (Element) XPath.selectSingleNode(navJdom, xpath.toString());
         }
-
-        log.debug("Processing navigation path "+path);
+        String contentPage = navitem.getAttributeValue("href");
+        String extern = navitem.getAttributeValue("extern");
+        nodeID = navitem.getAttributeValue("systemID");
         
-        if (e.isExtern()) {
-       		response.sendRedirect(e.getPage());
+        if ( extern != null && extern.equals("true")) {
+       		response.sendRedirect(navitem.getAttributeValue("href"));
         	return;
-        }
+        }        
         
-        tree.flag(path);
+        int i = 0;
+        while(navitem != null) {
+        	if (!navitem.getName().equals("navitem")) break;
+        	if (i != 0) {
+        		pathSB.insert(0,".");
+        		pathID.insert(0,".");
+        	}
+        	pathSB.insert(0, navitem.getAttributeValue("name"));
+        	pathID.insert(0,navitem.getAttributeValue("systemID"));
+        	Element youAreHereElement = (Element)navitem.clone();
+        	youAreHereElement.removeChildren("navitem");
+        	youAreHere.addContent(0,youAreHereElement);
+        	navitem =  navitem.getParentElement();
+        	i++;
+        }
+    	org.w3c.dom.Document domYouAreHere = null;
+		try {
+			domYouAreHere = new org.jdom.output.DOMOutputter().output(new org.jdom.Document(youAreHere));
+		} catch (org.jdom.JDOMException e) {
+			Logger.getLogger(NavServlet.class).error("Domoutput failed: ", e);
+		}  
+        path = pathSB.toString();        
+        
 
-        request.setAttribute("node", node);
+        logger.debug("Processing navigation path " + path);
+        logger.debug("Processing navigation pathID " + pathID.toString());
+        logger.debug("Processing navigation nodeID " + nodeID);
+        
+        request.setAttribute("nodeID", nodeID);
         request.setAttribute("path", path);
-
-        request.setAttribute("nav", tree);
-
-        if(e==null || e.getPage()==null) {
+        request.setAttribute("pathID", pathID.toString());
+        request.setAttribute("youAreHere",domYouAreHere);
+        
+        if(contentPage == null || contentPage.equals("")) {
             request.setAttribute("content", "content/dummy.jsp");
         } else {
-            request.setAttribute("content", e.getPage());
+            request.setAttribute("content", contentPage);
         }
         //response.setHeader("Expires", "Thu, 08 Dec 2005 16:00:00 GMT");
-        context.getRequestDispatcher("/frame.jsp").forward(request, response);
+        try{
+        	context.getRequestDispatcher("/frame.jsp").forward(request, response);
+        }catch(Exception e) {
+        	deinitialize();
+        	logger.error("catched error: ", e);
+        }
+        
     }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        doGet(request, response);
-    }
-    protected void doGetPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        doGet(request, response);
-    }    
 }
