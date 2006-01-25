ef1245fb51e50e889dd328f94c242bcf5cc34fac
Heiko Helmbrecht <mcrhhelm@duepub-1.zim.uni-duisburg-essen.de>
2006-01-25 13:26:39 +0000

    change in instanciating MCRAccessManager
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@6859 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/jsp/taglibs/MCRCheckAccessTag.java
+++ sources/org/mycore/frontend/jsp/taglibs/MCRCheckAccessTag.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.tagext.*;
 import javax.servlet.jsp.*;
-import org.mycore.access.MCRAccessManager;
+import org.mycore.access.MCRAccessManagerBase;
+import org.mycore.common.MCRConfiguration;
 import org.mycore.common.MCRSession;
 import org.mycore.frontend.servlets.MCRServlet;
 
@@ -14,6 +15,8 @@ public class MCRCheckAccessTag extends SimpleTagSupport
 	private String pool;
 	private String var;
 	private String key;
+	
+	private static MCRAccessManagerBase AI = (MCRAccessManagerBase) MCRConfiguration.instance().getInstanceOf("MCR.Access_class_name");
 
 	public void setPool(String inputPool) {
 		pool = inputPool;
@@ -30,7 +33,7 @@ public class MCRCheckAccessTag extends SimpleTagSupport
 	public void doTag() throws JspException, IOException {
 		PageContext pageContext = (PageContext) getJspContext();
 		MCRSession mcrSession = MCRServlet.getSession((HttpServletRequest)pageContext.getRequest());
-		boolean accessAllowed = MCRAccessManager.checkAccess(pool, key, mcrSession);
+		boolean accessAllowed = AI.checkAccess(pool, key, mcrSession);
 		if(accessAllowed)
 			pageContext.setAttribute(var, "true");
 		else
