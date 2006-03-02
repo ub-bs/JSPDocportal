11e6d4e9552633aeb9f36656a86a29a17a2bf9ae
Anja Helmbrecht-Schaar <mcrascha@duepub-1.zim.uni-duisburg-essen.de>
2006-03-02 12:09:57 +0000

    doTag, get XML either from the database or from the workflowfile for a preview
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@7404 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/jsp/taglibs/MCRReceiveMcrObjAsJdomTag.java
+++ sources/org/mycore/frontend/jsp/taglibs/MCRReceiveMcrObjAsJdomTag.java
@@ -1,5 +1,6 @@
 package org.mycore.frontend.jsp.taglibs;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 
@@ -9,6 +10,7 @@ import javax.servlet.jsp.*;
 import org.apache.log4j.Logger;
 import org.jdom.JDOMException;
 import org.jdom.output.DOMOutputter;
+import org.mycore.common.MCRConfiguration;
 import org.mycore.frontend.jsp.format.MCRResultFormatter;
 
 
@@ -16,6 +18,7 @@ public class MCRReceiveMcrObjAsJdomTag extends SimpleTagSupport
 {
 	private String mcrid;
 	private String var;
+	private String fromWForDB;
 	
 	public void setMcrid(String inputID) {
 		mcrid = inputID;
@@ -25,9 +28,25 @@ public class MCRReceiveMcrObjAsJdomTag extends SimpleTagSupport
 		var = inputVar;
 		return;
 	}
+	
+	public void setFromWForDB(String inputFromWForDB) {
+		fromWForDB = inputFromWForDB;
+	}
+	
 	public void doTag() throws JspException, IOException {
 		org.mycore.datamodel.metadata.MCRObject mcr_obj = new org.mycore.datamodel.metadata.MCRObject();
-		mcr_obj.receiveFromDatastore(mcrid);
+		if ( fromWForDB != null && fromWForDB.equals("workflow") ) {
+			MCRConfiguration CONFIG = MCRConfiguration.instance();
+			String[] mcridParts = mcrid.split("_");
+			String savedir = CONFIG.getString("MCR.editor_" + mcridParts[1] + "_directory");
+			String filename = savedir + "/" + mcrid + ".xml";			
+			File file = new File(filename);
+			if (file.isFile()) {
+				mcr_obj.setFromURI(file.getAbsolutePath());
+			}
+		} else {
+			mcr_obj.receiveFromDatastore(mcrid);
+		}
 		PageContext pageContext = (PageContext) getJspContext();
 		pageContext.setAttribute(var, mcr_obj.createXML());
 		return;
