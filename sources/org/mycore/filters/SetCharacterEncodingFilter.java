a009dd4a6f40d2d5effba6a6d7caf3c01a48eb30
Heiko Helmbrecht <mcrhhelm@duepub-1.zim.uni-duisburg-essen.de>
2006-03-29 22:02:59 +0000

    added a encoding filter
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@7769 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ sources/org/mycore/filters/SetCharacterEncodingFilter.java
@@ -0,0 +1,42 @@
+package org.mycore.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public final class SetCharacterEncodingFilter implements Filter {
+
+	private FilterConfig filterConfig = null;
+	private String encoding = null;
+	
+	public void init(FilterConfig filterConfig) throws
+	   ServletException {
+	   this.filterConfig = filterConfig;
+	   this.encoding = filterConfig.getInitParameter("encoding");
+	}
+
+	public void doFilter(ServletRequest request, 
+				ServletResponse response, FilterChain chain) throws
+				IOException, ServletException {
+		String encoding = selectEncoding(request);
+		System.out.println("coming encoding = " + encoding);
+		if (encoding != null)
+			request.setCharacterEncoding(encoding);
+		chain.doFilter(request, response);
+	}
+
+	protected String selectEncoding(ServletRequest request) {
+		return (this.encoding);
+	}	
+
+	public void destroy() {
+		this.filterConfig = null;
+		this.encoding = null;
+	}
+
+}
