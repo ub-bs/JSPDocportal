5e1177e56aeef5c471aec2f2e16136fc1beb38cb
Heiko Helmbrecht <mcrhhelm@duepub-1.zim.uni-duisburg-essen.de>
2006-03-31 08:18:33 +0000

    fix a Cannot serialize session attribute bug
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@7806 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/workflowengine/jbpm/MCRJbpmTaskBean.java
+++ sources/org/mycore/frontend/workflowengine/jbpm/MCRJbpmTaskBean.java
@@ -1,8 +1,11 @@
 package org.mycore.frontend.workflowengine.jbpm;
 
+import java.io.Serializable;
 
-public class MCRJbpmTaskBean {
 
+public class MCRJbpmTaskBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 	private long processID;
 	private String workflowProcessType;
 	private String taskName;
