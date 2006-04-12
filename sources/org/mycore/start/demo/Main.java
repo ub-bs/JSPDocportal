7d38e9341073a645dff165417ac28ad96dbdca7a
Heiko Helmbrecht <mcrhhelm@duepub-1.zim.uni-duisburg-essen.de>
2006-04-12 18:01:27 +0000

    the automatic demo application is now startable without manual starting ofanother programs/servers like hsqldb or jetty
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@8028 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/start/demo/Main.java
+++ sources/org/mycore/start/demo/Main.java
@@ -1,7 +1,11 @@
 package org.mycore.start.demo;
 
 import java.io.File;
-import org.mortbay.jetty.Server;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import org.apache.log4j.Logger;
 
 import edu.stanford.ejalbert.BrowserLauncher;
 import edu.stanford.ejalbert.BrowserLauncherRunner;
@@ -20,6 +24,7 @@ public class Main {
 
 	final static String NL = System.getProperty("line.separator");
 	private String jettyHome;
+	private String jettyHomeAbsolutePath;
 
 	public Main() {
 		super();
@@ -34,7 +39,8 @@ public class Main {
 		String curDir = System.getProperty("user.dir");
 		File jetty = new File(curDir + File.separator + "mycore-working" + File.separator + "lib" + File.separator + "jetty");
 		if(jetty.exists()){
-			jettyHome = jetty.getAbsolutePath().replaceAll("\\\\","/");
+			jettyHomeAbsolutePath = jetty.getAbsolutePath();
+			jettyHome = jettyHomeAbsolutePath.replaceAll("\\\\","/");
 			System.out.println("jetty-home=" + jettyHome);
 		}else{
 			System.err.println("Demo.jar is not in the right directory");
@@ -54,20 +60,21 @@ public class Main {
 	 */
     public static void main(String[] args)
     {
-
         try
         {
         	Main main = new Main();
         	if(args.length <= 0) {
-        		
+        		System.err.println("Usage (LINUX): %JDK_HOME%/bin/java -jar demo.jar start");
+            	System.err.println("Usage (WINDOWS): %JDK_HOME%\\bin\\java -jar demo.jar start");
+            	System.exit(1);        		
         	}else if (args[0].equals("start")){
         		main.startJetty();
-        		Thread.sleep(2000);
+        		Thread.sleep(5000);
         		main.launchBrowser("http://127.0.0.1:8085/mycoresample/");
-        	}else if (args[0].equals("help")){
-        		System.err.println("Usage (LINUX): %JDK_HOME%/bin/java -jar demo.jar [--help] [start]");
-            	System.err.println("Usage (WINDOWS): %JDK_HOME%\\bin\\java -jar demo.jar [--help] [start]");
-            	System.exit(1);        		
+        	}else {
+        		System.err.println("Usage (LINUX): %JDK_HOME%/bin/java -jar demo.jar start");
+            	System.err.println("Usage (WINDOWS): %JDK_HOME%\\bin\\java -jar demo.jar start");
+            	System.exit(1);
         	}
         }
         catch(Exception e)
@@ -77,21 +84,54 @@ public class Main {
     }
     
     public void startJetty(){
-		Server server = new Server();
-		try {
-			System.setProperty("jetty.home",jettyHome);
-			server.configure(jettyHome + "/webapp.xml");
-			server.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    	Thread jettyThread = new Thread( new JettyRunnable(jettyHome, jettyHomeAbsolutePath) ); 
+		jettyThread.start();    	
     }
-
+    
+//    public void startJetty(){
+//		Server server = new Server();
+//		try {
+//			System.setProperty("jetty.home",jettyHome);
+//			server.configure(jettyHome + "/webapp.xml");
+//			server.start();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//    }
+    
     public void launchBrowser(String url) throws Exception {
 
 		BrowserLauncher launcher = new BrowserLauncher(null);
 		BrowserLauncherRunner runner = new BrowserLauncherRunner(launcher, url, null);
 		Thread launcherThread = new Thread(runner);
 		launcherThread.start();
+    }
+    
+    public static class JettyRunnable implements Runnable{
+    	private String jettyHome;
+    	private String jettyHomeAbsolutePath;
+    	public JettyRunnable(String jettyHome, String jettyHomeAbsolutePath){
+    		this.jettyHome = jettyHome;
+    		this.jettyHomeAbsolutePath = jettyHomeAbsolutePath;
+    	}
+    	public void run() {
+	    	try{
+	    		// without this complicated procedere jsp-file cannot be compiled and tools.jar is not found
+	    		System.setProperty("jetty.home", jettyHome);
+	    		URL[] urls = {new File(jettyHomeAbsolutePath + File.separator + "start.jar").toURL()};
+	    		URLClassLoader ucl = new URLClassLoader(urls);
+	    		Class cl = ucl.loadClass("org.mortbay.start.Main");
+	    		Class[] classes = new Class[1];
+	    		classes[0] = String[].class;
+	    		Method main = cl.getDeclaredMethod("main", classes);
+	    		Object[] methodParams = new Object[1];
+	    		String[] arguments = { jettyHomeAbsolutePath + File.separator + "webapp.xml"};
+	    		methodParams[0] = (Object) arguments;
+	    		main.invoke(null, methodParams);    		
+	    	}catch(Exception e){
+	    		Logger.getLogger(JettyRunnable.class).error("stacktrace", e);
+	    	}			
+		}
+    	
     }
 }
