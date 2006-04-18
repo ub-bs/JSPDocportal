918e0d9a6ae7a2d41e0b95ee7f2ff355010accdf
Heiko Helmbrecht <mcrhhelm@duepub-1.zim.uni-duisburg-essen.de>
2006-04-18 07:57:13 +0000

    fix jetty start of the demo-application
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@8044 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/start/demo/Main.java
+++ sources/org/mycore/start/demo/Main.java
@@ -22,9 +22,12 @@ import edu.stanford.ejalbert.BrowserLauncherRunner;
  */
 public class Main {
 
+	final static String deactivateSuffix = ".notallowedinjetty" ;
+	final static String[] xmlLibs = {"xercesImpl_2_7_1.jar" , "xml-apis.jar" };
 	final static String NL = System.getProperty("line.separator");
 	private String jettyHome;
 	private String jettyHomeAbsolutePath;
+	private String baseDir;
 
 	public Main() {
 		super();
@@ -36,8 +39,8 @@ public class Main {
 			System.err.println("JAVA_HOME is not set, or does not point to your JDK");
 			System.exit(-1);
 		}
-		String curDir = System.getProperty("user.dir");
-		File jetty = new File(curDir + File.separator + "mycore-working" + File.separator + "lib" + File.separator + "jetty");
+		baseDir = System.getProperty("user.dir");
+		File jetty = new File(baseDir + File.separator + "mycore-working" + File.separator + "lib" + File.separator + "jetty");
 		if(jetty.exists()){
 			jettyHomeAbsolutePath = jetty.getAbsolutePath();
 			jettyHome = jettyHomeAbsolutePath.replaceAll("\\\\","/");
@@ -68,9 +71,14 @@ public class Main {
             	System.err.println("Usage (WINDOWS): %JDK_HOME%\\bin\\java -jar demo.jar start");
             	System.exit(1);        		
         	}else if (args[0].equals("start")){
+        		main.renameXmlLibs("start");
         		main.startJetty();
         		Thread.sleep(5000);
         		main.launchBrowser("http://127.0.0.1:8085/mycoresample/");
+        	}else if (args[0].equals("stop")){
+        		main.stopJetty();
+        		Thread.sleep(5000);
+        		main.renameXmlLibs("stop");
         	}else {
         		System.err.println("Usage (LINUX): %JDK_HOME%/bin/java -jar demo.jar start");
             	System.err.println("Usage (WINDOWS): %JDK_HOME%\\bin\\java -jar demo.jar start");
@@ -83,10 +91,39 @@ public class Main {
         }
     }
     
+    public void renameXmlLibs(String mode){
+    	String basedirLIBS = new StringBuffer(baseDir).append(File.separator)
+    		.append("mycore-working").append(File.separator)
+    		.append("webapps").append(File.separator)
+    		.append("mycoresample").append(File.separator)
+    		.append("WEB-INF").append(File.separator)
+    		.append("lib").append(File.separator).toString();
+    	if(mode.equals("start")){
+    		for (int i = 0; i < xmlLibs.length; i++) {
+				File file = new File(basedirLIBS + xmlLibs[i]);
+				if(file.exists()){
+					file.renameTo(new File(basedirLIBS + xmlLibs[i] + deactivateSuffix));
+				}
+			}
+    	}else if(mode.equals("stop")){
+    		for (int i = 0; i < xmlLibs.length; i++) {
+				File file = new File(basedirLIBS + xmlLibs[i] + deactivateSuffix);
+				if(file.exists()){
+					file.renameTo(new File(basedirLIBS + xmlLibs[i]));
+				}
+			}    		
+    	}
+    }
+    
     public void startJetty(){
-    	Thread jettyThread = new Thread( new JettyRunnable(jettyHome, jettyHomeAbsolutePath) ); 
+    	Thread jettyThread = new Thread( new JettyRunnable(jettyHome, jettyHomeAbsolutePath, "start") ); 
 		jettyThread.start();    	
     }
+    
+    public void stopJetty(){
+    	Thread jettyThread = new Thread( new JettyRunnable(jettyHome, jettyHomeAbsolutePath, "stop") ); 
+		jettyThread.start();    	
+    }    
     
 //    public void startJetty(){
 //		Server server = new Server();
@@ -110,26 +147,29 @@ public class Main {
     public static class JettyRunnable implements Runnable{
     	private String jettyHome;
     	private String jettyHomeAbsolutePath;
-    	public JettyRunnable(String jettyHome, String jettyHomeAbsolutePath){
+    	private String mode;
+    	public JettyRunnable(String jettyHome, String jettyHomeAbsolutePath, String mode){
     		this.jettyHome = jettyHome;
     		this.jettyHomeAbsolutePath = jettyHomeAbsolutePath;
+    		// mode: start|stop
+    		this.mode = mode;
     	}
     	public void run() {
 	    	try{
 	    		// without this complicated procedere jsp-file cannot be compiled and tools.jar is not found
 	    		System.setProperty("jetty.home", jettyHome);
-	    		URL[] urls = {new File(jettyHomeAbsolutePath + File.separator + "start.jar").toURL()};
+	    		URL[] urls = {new File(jettyHomeAbsolutePath + File.separator + mode + ".jar").toURL()};
 	    		URLClassLoader ucl = new URLClassLoader(urls);
-	    		Class cl = ucl.loadClass("org.mortbay.start.Main");
+	    		Class cl = ucl.loadClass("org.mortbay." + mode + ".Main");
 	    		Class[] classes = new Class[1];
 	    		classes[0] = String[].class;
 	    		Method main = cl.getDeclaredMethod("main", classes);
 	    		Object[] methodParams = new Object[1];
-	    		String[] arguments = { jettyHomeAbsolutePath + File.separator + "webapp.xml"};
-	    		methodParams[0] = (Object) arguments;
-	    		main.invoke(null, methodParams);    		
+    			String[] arguments = { jettyHomeAbsolutePath + File.separator + "webapp.xml"};
+    			methodParams[0] = (Object) arguments;
+	    		main.invoke(null, methodParams);  
 	    	}catch(Exception e){
-	    		Logger.getLogger(JettyRunnable.class).error("stacktrace", e);
+	    		e.printStackTrace();
 	    	}			
 		}
     	
