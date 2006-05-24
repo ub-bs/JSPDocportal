21005b8096a5eb572b8180bb86eb1ee7fad71931
Anja Helmbrecht-Schaar <mcrascha@duepub-1.zim.uni-duisburg-essen.de>
2006-05-24 14:26:32 +0000

    complete user jDom
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@8496 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/jsp/taglibs/MCRReceiveUserAsJdomTag.java
+++ sources/org/mycore/frontend/jsp/taglibs/MCRReceiveUserAsJdomTag.java
@@ -8,6 +8,7 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 import org.apache.log4j.Logger;
 import org.jdom.Document;
+import org.jdom.Element;
 import org.jdom.output.DOMOutputter;
 import org.mycore.common.MCRException;
 import org.mycore.common.MCRSessionMgr;
@@ -15,7 +16,7 @@ import org.mycore.user2.MCRUser;
 import org.mycore.user2.MCRUserMgr;
 public class MCRReceiveUserAsJdomTag extends SimpleTagSupport
 {
-	private static Logger logger = Logger.getLogger(MCRReceiveMcrObjAsJdomTag.class);
+	private static Logger logger = Logger.getLogger(MCRReceiveUserAsJdomTag.class);
 	
 	private String userID;
 	private String var ;
@@ -43,16 +44,16 @@ public class MCRReceiveUserAsJdomTag extends SimpleTagSupport
 				u.setDescription(sNoUser);
 			}
 			
-			Document jUser = u.toJDOMDocument();    		
+			Document jUser = u.toJDOMDocument();    	
+			Element eUser = (Element)jUser.getRootElement().getChild("user").clone();
 			PageContext pageContext = (PageContext) getJspContext();
 	    	//org.w3c.dom.Document domDoc = null;
     		//domDoc =  new DOMOutputter().output( jUser);
     		//pageContext.setAttribute(var , domDoc);
-    		pageContext.setAttribute(var , jUser);
-
+    		pageContext.setAttribute(var , new Document(eUser));
     		
     	} catch (Exception e) {
-    		logger.error("error in receiving mcr_obj for jdom and dom", e);
+    		logger.error("error in receiving user for jdom ", e);
     	}
 	}	
 
