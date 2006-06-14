7e0f7714bf7b62a6a35a0d6f8065eee0ae7479b3
Robert Stephan <rsteph-de@users.noreply.github.com>
2006-06-14 11:28:11 +0000

    Simplify AuthorEditDialog
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@8835 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/workflowengine/strategies/MCRDefaultAuthorStrategy.java
+++ sources/org/mycore/frontend/workflowengine/strategies/MCRDefaultAuthorStrategy.java
@@ -60,7 +60,7 @@ public class MCRDefaultAuthorStrategy implements MCRAuthorStrategy {
 			}
 		} catch ( Exception ex){
 			//TODO Fehlermeldung
-			logger.warn("Could not Create authors object for the user:  " + userid);
+			logger.warn("Could not Create authors object for the user:  " + userid, ex);
 			return null;
 		}
    	    return author.getId();		
@@ -169,8 +169,29 @@ public class MCRDefaultAuthorStrategy implements MCRAuthorStrategy {
 			metadata.addContent(ePnames);
 			metadata.addContent(eFemales);
 			metadata.addContent(ePadrs);
-			metadata.addContent(eUserIDs);
 			metadata.addContent(eEmails);
+			metadata.addContent(eUserIDs);
+			
+//			<xsd:sequence>
+//            <xsd:element maxOccurs="1" minOccurs="1" name="names" type="namesType"/>
+//            <xsd:element maxOccurs="1" minOccurs="0" name="females" type="femalesType"/>
+//            <xsd:element maxOccurs="1" minOccurs="1" name="institutions" type="institutionsType"/>
+//            <xsd:element maxOccurs="1" minOccurs="0" name="addresses" type="addressesType"/>
+//            <xsd:element maxOccurs="1" minOccurs="0" name="phones" type="phonesType"/>
+//            <xsd:element maxOccurs="1" minOccurs="0" name="dates" type="datesType"/>
+//            <xsd:element maxOccurs="1" minOccurs="0" name="professions" type="professionsType"/>
+//            <xsd:element maxOccurs="1" minOccurs="0" name="profclasses" type="profclassesType"/>
+//            <xsd:element maxOccurs="1" minOccurs="0" name="nationals" type="nationalsType"/>
+//            <xsd:element maxOccurs="1" minOccurs="0" name="urls" type="urlsType"/>
+//            <xsd:element maxOccurs="1" minOccurs="0" name="emails" type="emailsType"/>
+//            <xsd:element maxOccurs="1" minOccurs="0" name="references" type="referencesType"/>
+//            <xsd:element maxOccurs="1" minOccurs="0" name="notes" type="notesType"/>
+//            <xsd:element maxOccurs="1" minOccurs="0" name="publications" type="publicationsType"/>
+//            <xsd:element maxOccurs="1" minOccurs="0" name="userids" type="useridsType"/>
+//			<xsd/:sequence>
+			
+			
+			
 			
 		} else{
 			//metadata needs dummy data otherwise  author.createXML() failes
@@ -185,6 +206,10 @@ public class MCRDefaultAuthorStrategy implements MCRAuthorStrategy {
 			ePnames.addContent(pname.createXML());
 						
 			metadata.addContent(ePnames);
+	//		metadata.addContent(new Element("females"));
+	//		metadata.addContent(new Element("institutions"));
+	//		metadata.addContent(new Element("addresses"));
+			
 		}
 
 		xmlElemAuthor.addContent(structure);
@@ -197,7 +222,9 @@ public class MCRDefaultAuthorStrategy implements MCRAuthorStrategy {
 
 		// Robert: redundant?
 		author.setId(id);
-
+			
+		XMLOutputter xout = new XMLOutputter(Format.getPrettyFormat());
+		Logger.getLogger(this.getClass()).info(xout.outputString(author.createXML()));
 		return author;
 	}		
 }
