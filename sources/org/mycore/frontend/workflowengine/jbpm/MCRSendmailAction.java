433f5ebac870d3cd50ad57080f2c6ec31eafaaa1
Anja Helmbrecht-Schaar <mcrascha@duepub-1.zim.uni-duisburg-essen.de>
2006-07-25 11:20:09 +0000

    Configuration for workflow Email
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@9154 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/workflowengine/jbpm/MCRSendmailAction.java
+++ sources/org/mycore/frontend/workflowengine/jbpm/MCRSendmailAction.java
@@ -2,6 +2,7 @@ package org.mycore.frontend.workflowengine.jbpm;
 
 import org.jbpm.graph.def.ActionHandler;
 import org.jbpm.graph.exe.ExecutionContext;
+import org.mycore.common.MCRConfiguration;
 import org.mycore.common.MCRException;
 
 public class MCRSendmailAction implements ActionHandler{
@@ -22,7 +23,18 @@ public class MCRSendmailAction implements ActionHandler{
 	
 	
 	public void execute(ExecutionContext executionContext) throws MCRException {
-		MCRJbpmSendmail.sendMail(from, to, replyTo, bcc, subject,
+		String myreplyTo="";
+		String mybcc ="";
+		
+		//Either in the variable is the ConfigString or the real value
+		String myfrom = MCRConfiguration.instance().getString(from,from);
+		String myto = MCRConfiguration.instance().getString(to,to);
+		if ( replyTo != null )
+			myreplyTo = MCRConfiguration.instance().getString(replyTo,replyTo);
+		if ( bcc != null)
+			mybcc = MCRConfiguration.instance().getString(bcc,bcc);
+		
+		MCRJbpmSendmail.sendMail(myfrom, myto, myreplyTo, mybcc, subject,
 				body, mode, jbpmVariableName, dateOfSubmissionVariable, executionContext);
 	}
 }
