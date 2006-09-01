be0590f1847bfedfb724afa0e1d3c7801aebd9cf
Anja Helmbrecht-Schaar <mcrascha@duepub-1.zim.uni-duisburg-essen.de>
2006-09-01 13:14:27 +0000

    extends MCRSearchServlet and overides jsp dependend functionality, used for searching and browsing....(resort, refine, new query + MCRSearchServlet)
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@9605 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/servlets/MCRJSPSearchServlet.java
+++ sources/org/mycore/frontend/servlets/MCRJSPSearchServlet.java
@@ -24,84 +24,163 @@
 package org.mycore.frontend.servlets;
 
 import java.io.IOException;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Vector;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
 import org.jdom.Document;
 import org.jdom.Element;
-import org.jdom.filter.ElementFilter;
 import org.jdom.output.XMLOutputter;
-import org.mycore.common.JSPUtils;
-import org.mycore.common.MCRCache;
-import org.mycore.common.MCRConfiguration;
+import org.mycore.common.MCRSession;
 import org.mycore.common.MCRSessionMgr;
-import org.mycore.frontend.editor.MCREditorSubmission;
 import org.mycore.parsers.bool.MCRCondition;
 import org.mycore.services.fieldquery.MCRQuery;
 import org.mycore.services.fieldquery.MCRQueryManager;
-import org.mycore.services.fieldquery.MCRQueryParser;
 import org.mycore.services.fieldquery.MCRResults;
 import org.mycore.services.fieldquery.MCRSearchServlet;
 
 /**
  * This servlet executes queries and presents result pages.
  * 
- * @author Frank Lützenkirchen
- * @author Harald Richter
+ * @author Anja Schaar
+ * 
  */
 public class MCRJSPSearchServlet extends MCRSearchServlet {
     private static final long serialVersionUID = 1L;
 
-    private static String resultlistType = "simple";
+    private String resultlistType = "simple";
+    
+    public void doGetPost(MCRServletJob job) throws IOException, ServletException {
+        String mode = job.getRequest().getParameter("mode");
+        
+        if ("resort".equals(mode))
+        	resortQuery(job.getRequest(), job.getResponse());
+        else if ("refine".equals(mode))
+        	refineQuery(job.getRequest(), job.getResponse());
+        else if ("renew".equals(mode))
+        	renewQuery(job.getRequest(), job.getResponse());
+        else {
+        	super.doGetPost(job);
+        }
+    }
+
+    // this calls the editor of the searchmask with the inputfield
+    private void refineQuery(HttpServletRequest req, HttpServletResponse res) throws IOException,ServletException {
+    	String furl = "/nav?path=";    
+		String id = req.getParameter("id");
+		String editormask = req.getParameter("mask");
+		MCRSession session = MCRSessionMgr.getCurrentSession();
+		
+		if ( editormask.startsWith("~searchstart-class")) {
+			// this comes from the browserclass and we have no searchmask
+			String browseuri  = session.BData.getUri();
+			furl +=editormask+"&actUriPath="+browseuri; 
+		} else {
+		   // we must set the session, cause in the next request for the editor call we need the right 
+			// session to get the query from the cache
+			furl += editormask+"&sourceid="+id+"&session="+session.getID();
+		}
+		this.getServletContext().getRequestDispatcher(furl).forward(req, res);
+    }
+    
+    // this calls the editor start address of the searchmask with the inputfield or the classbrowser start adresse
+    private void renewQuery(HttpServletRequest req, HttpServletResponse res) throws IOException ,ServletException {
+		String editormask = req.getParameter("mask");
+    	String furl = "/nav?path="+editormask;    
+		this.getServletContext().getRequestDispatcher(furl).forward(req, res);
+    }    
+    
+    //this mode comes from the resort form in the resultlist
+    private void resortQuery(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    	// the id of the query
+		String id = req.getParameter("id");
+		Document query = (Document) (getCache(getResortKey()).get(id));
+		Document origquery = (Document) (getCache(getQueriesKey()).get(id));
+		MCRCondition cond = (MCRCondition) (getCache(getConditionsKey()).get(id));
+		
+		Element sortBy = new Element("sortBy");
+		int i = 1;
+		for ( i = 1; i < 4; i++) {
+			if (req.getParameter("field" + i) != null && !req.getParameter("field" + i).equals("")) {
+				Element sortField = new Element("field");
+				sortField.setAttribute("name",req.getParameter("field" + i));
+				String order = (req.getParameter("order" + i) != null) ?
+						req.getParameter("order" + i) : "ascending" ;
+				sortField.setAttribute("order",order);
+				sortBy.addContent(sortField);
+			} else {break;}
+		}
+		
+		if ( i != 1) {
+			query.getRootElement().removeChild("sortBy");
+			query.getRootElement().addContent(sortBy);
+		}
+		
+        // Execute query
+        long start = System.currentTimeMillis();
+        MCRResults result = MCRQueryManager.search(MCRQuery.parseXML(query));
+        long qtime = System.currentTimeMillis() - start;
+        LOGGER.debug("MCRJSPSearchServlet total query time: " + qtime);
+
+        String npp = query.getRootElement().getAttributeValue("numPerPage", "0");
+
+        // Store query and results in cache
+        getCache(getResultsKey()).put(result.getID(), result);
+        getCache(getResortKey()).put(result.getID(), query);
+        getCache(getQueriesKey()).put(result.getID(), origquery); 
+        getCache(getConditionsKey()).put(result.getID(), cond);
+        
+        // Redirect browser to first results page
+        sendRedirect(req, res, result.getID(), npp);
+
+    }
+    
     /** 
      *  Forwards the document to the output
      *  @author A.Schaar
-     *  @see its overwritten in jspdocportal 
+     *  @see its from mycore and overwritten here 
      */
     protected void forwardRequest(HttpServletRequest req, HttpServletResponse res, Document jdom) throws IOException, ServletException {
-        req.setAttribute("query", jdom);
-        this.getServletContext().getRequestDispatcher("/nav?path=~searchstart-" + resultlistType).forward(req, res);        
-    }
+    	if ( "results".equalsIgnoreCase(jdom.getRootElement().getName()) ) {
+    		String path = "/nav?path=";
 
+    		//<mcr:results xmlns:mcr="http://www.mycore.org/" id="1iljqgz8zqp6merg8xiel" 
+            // Show incoming result document
+            if (LOGGER.isDebugEnabled()) {
+                XMLOutputter out = new XMLOutputter(org.jdom.output.Format.getPrettyFormat());
+                LOGGER.debug(out.outputString(jdom));
+            }
+    		String id = jdom.getRootElement().getAttributeValue("id");
+    		String mask = jdom.getRootElement().getAttributeValue("mask");
+    		Document query = (Document) (getCache(getResortKey()).get(id));
+    		req.setAttribute("query", query);
+    		req.setAttribute("results", jdom);
+    		
+    		String[] maskarray = mask.split("-");
+    		
+    		if ( maskarray.length > 0 )
+    			 resultlistType = maskarray[1];
+    		else resultlistType = "simple";
+
+   			path += "~searchresult-" + resultlistType;
+    		this.getServletContext().getRequestDispatcher(path).forward(req, res);	
+    	}
+    	else {
+    		// reload the searchmask with in the query
+    		super.forwardRequest(req,res,jdom);
+    	}
+    }
+    
     /** 
      *  Redirect browser to results page
      *  @author A.Schaar
      *  @see its overwritten in jspdocportal 
      */
-    protected void sendRedirect( HttpServletRequest req, HttpServletResponse res, String id, String numPerPage) throws IOException{
-// 		Redirect browser to first results page
-//	    String url = "MCRSearchServlet?mode=results&id=" + id + "&numPerPage=" + numPerPage;
-//	    res.sendRedirect(res.encodeRedirectURL(url));
-
-    	
-    	Document query = (Document) (getCache(getQueriesKey()).get(id));
-    	
-	    String resultlistType = "simple";
-		if (query.getRootElement().getAttributeValue("resultlistType") != null) {
-		    	resultlistType = query.getRootElement().getAttributeValue("resultlistType");
-		    	query.getRootElement().removeAttribute("resultlistType") ;
-		    }
-	    req.setAttribute("query", query);
-
-		req.setAttribute("resultlistType",resultlistType);
-	    req.setAttribute("mode","results");
-	    req.setAttribute("id",id);
-	    req.setAttribute("numPerPage",numPerPage);
-	    String url = "/nav?path=~searchresult-" + resultlistType;
-	    try {
-	    	this.getServletContext().getRequestDispatcher(url).forward(req, res);
-	    } catch ( ServletException sx) {
-	    	;
-	    }
+    protected void sendRedirect( HttpServletRequest req, HttpServletResponse res, String id, String numPerPage) throws IOException {
+	    // Redirect browser to first results page
+    	MCRSession session = MCRSessionMgr.getCurrentSession();
+	    String url = "MCRJSPSearchServlet?mode=results&id=" + id + "&numPerPage=" + numPerPage + "&session="+session.getID();
+	    res.sendRedirect(res.encodeRedirectURL(url));
     }
-
-
 }
