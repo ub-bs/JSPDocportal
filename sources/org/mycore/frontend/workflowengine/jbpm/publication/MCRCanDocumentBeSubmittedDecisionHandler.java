113dbcb516f87d10e9367ed7153e20bf5525fb12
Anja Helmbrecht-Schaar <mcrascha@duepub-1.zim.uni-duisburg-essen.de>
2006-10-04 09:18:39 +0000

    load correct classes for WFM
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@10011 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/workflowengine/jbpm/publication/MCRCanDocumentBeSubmittedDecisionHandler.java
+++ sources/org/mycore/frontend/workflowengine/jbpm/publication/MCRCanDocumentBeSubmittedDecisionHandler.java
@@ -3,13 +3,14 @@ package org.mycore.frontend.workflowengine.jbpm.publication;
 import org.apache.log4j.Logger;
 import org.jbpm.graph.exe.ExecutionContext;
 import org.jbpm.graph.node.DecisionHandler;
+import org.mycore.frontend.workflowengine.jbpm.MCRWorkflowManager;
 import org.mycore.frontend.workflowengine.jbpm.MCRWorkflowManagerFactory;
-import org.mycore.frontend.workflowengine.jbpm.author.MCRWorkflowManagerAuthor;
+
 
 public class MCRCanDocumentBeSubmittedDecisionHandler implements DecisionHandler {
 	private static final long serialVersionUID = 1L;
 	private static Logger logger = Logger.getLogger(MCRCanDocumentBeSubmittedDecisionHandler.class);
-	private static MCRWorkflowManagerAuthor WFM = (MCRWorkflowManagerAuthor)MCRWorkflowManagerFactory.getImpl("publication");
+	private static MCRWorkflowManager WFM = MCRWorkflowManagerFactory.getImpl("publication");
 	/**
 	* decides which transition to take. returnes the name of this transition
 	*/
