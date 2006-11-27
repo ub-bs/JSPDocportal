2d80b5f0b3bfe0840b9ffed22cc656bd889bfca9
Robert Stephan <rsteph-de@users.noreply.github.com>
2006-11-27 15:34:51 +0000

    update classification-to-item - use Java methods instead of XSLT - new parameter: emptyleafs=no to hide empty items
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@10603 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/editor/helper/MCRGetEditorElements.java
+++ sources/org/mycore/frontend/editor/helper/MCRGetEditorElements.java
@@ -1,3 +1,26 @@
+/*
+ * $RCSfile$
+ * $Revision$ $Date$
+ *
+ * This file is part of ***  M y C o R e  *** 
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
 package org.mycore.frontend.editor.helper;
 
 import java.io.IOException;
@@ -9,7 +32,6 @@ import java.util.Locale;
 import java.util.Properties;
 import java.util.PropertyResourceBundle;
 
-import javax.xml.transform.Source;
 import javax.xml.transform.TransformerException;
 
 import org.apache.log4j.Logger;
@@ -17,8 +39,6 @@ import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.filter.ElementFilter;
 import org.mycore.common.MCRConfiguration;
-import org.mycore.common.xml.MCRURIResolver;
-import org.mycore.common.xml.MCRXSLTransformation;
 import org.mycore.common.xml.MCRURIResolver.MCRResolver;
 import org.mycore.datamodel.classifications.MCRClassification;
 import org.mycore.user2.MCRUserMgr;
@@ -31,7 +51,8 @@ import org.mycore.user2.MCRUserMgr;
  * 
  * getClassificationItems
  * 
- * @author Helmbrecht
+ * @author Heiko Helmbrecht
+ * @author Robert Stephan
  *
  */
 public class MCRGetEditorElements implements MCRResolver {
@@ -124,8 +145,7 @@ public class MCRGetEditorElements implements MCRResolver {
 		String emptyLeafs = params.getProperty("emptyLeafs");		
 		if(emptyLeafs == null || emptyLeafs.equals("")){
 			emptyLeafs = "yes";
-		} else 
-			emptyLeafs = "no";
+		}
 		
         if(classid == null || classid.equals("")){
             String prop = params.getProperty("prop");
@@ -142,18 +162,13 @@ public class MCRGetEditorElements implements MCRResolver {
 	
 	private Element transformClassToItems(String classid, String emptyLeafs) throws TransformerException{
         Document classJdom = MCRClassification.receiveClassificationAsJDOM(classid);
-        Source xsl = MCRURIResolver.instance().resolve("webapp:WEB-INF/stylesheets/classifications-to-items.xsl","classifications-to-items.jspx");
-        
-        Properties myProps = new Properties();
-        myProps.setProperty("emptyLeafs", emptyLeafs);
-        
-        return MCRXSLTransformation.transform(classJdom, xsl, myProps).getRootElement();		
+        boolean displayEmptyLeafs = (emptyLeafs.equalsIgnoreCase("yes")||emptyLeafs.equalsIgnoreCase("true"));
+        return MCREditorClassificationHelper.transformClassificationtoItems(classJdom, displayEmptyLeafs).getRootElement();
 	}
 	
 	private Element transformClassLabelsToItems(String classid) throws TransformerException{
         Document classJdom = MCRClassification.receiveClassificationAsJDOM(classid);
-        Source xsl = MCRURIResolver.instance().resolve("webapp:WEB-INF/stylesheets/classificationsLabels-to-items.xsl","classifications-to-items.jspx");
-        return MCRXSLTransformation.transform(classJdom, xsl, new Properties()).getRootElement();		
+        return MCREditorClassificationHelper.transformClassificationLabeltoItems(classJdom, true).getRootElement();
 	}
 
 	private Element getSpecialCategoriesInItems(Properties params) throws TransformerException{
@@ -182,7 +197,6 @@ public class MCRGetEditorElements implements MCRResolver {
 				}
 			}			
 		}
-
 		return retitems;
 	}	
 	
