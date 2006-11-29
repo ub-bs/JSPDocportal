46dbe83af93d53d256382f4c0fecdfd66427dac3
Anja Helmbrecht-Schaar <mcrascha@duepub-1.zim.uni-duisburg-essen.de>
2006-11-29 15:35:07 +0000

    check no for guests
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@10644 7089d063-bc14-496d-ac90-53de1872eb6c

--- sources/org/mycore/frontend/jsp/taglibs/MCRCheckAccessTag.java
+++ sources/org/mycore/frontend/jsp/taglibs/MCRCheckAccessTag.java
@@ -1,12 +1,16 @@
 package org.mycore.frontend.jsp.taglibs;
 
 import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.tagext.*;
 import javax.servlet.jsp.*;
 
 import org.apache.log4j.Logger;
 import org.mycore.access.MCRAccessInterface;
 import org.mycore.access.MCRAccessManager;
+import org.mycore.common.MCRSession;
+import org.mycore.frontend.servlets.MCRServlet;
 
 public class MCRCheckAccessTag extends SimpleTagSupport
 {
@@ -31,8 +35,14 @@ public class MCRCheckAccessTag extends SimpleTagSupport
 	}	
 	public void doTag() throws JspException, IOException {
 		try{
-			PageContext pageContext = (PageContext) getJspContext();
-			if ( key == null || "".equals(key)) // allgemeiner check des aktuellen Users
+
+			PageContext pageContext = (PageContext) getJspContext();			
+			MCRSession mcrSession = MCRServlet.getSession((HttpServletRequest)pageContext.getRequest());
+			
+			if ( mcrSession.getCurrentUserID().equals("guest") )
+				pageContext.setAttribute(var, new Boolean(false));	
+			
+			else if ( key == null || "".equals(key)) // allgemeiner check des aktuellen Users
 				pageContext.setAttribute(var, new Boolean(AI.checkPermission(permission)));
 			else 
 				pageContext.setAttribute(var, new Boolean(AI.checkPermission(key, permission)));
