68593758a486e5298e4592dad6e4b93f7ce06b3b
Robert Stephan <rsteph-de@users.noreply.github.com>
2011-02-18 16:14:32 +0000

    searchresult browser - label for sorting hit list
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@19969 7089d063-bc14-496d-ac90-53de1872eb6c

--- jspdocportal-webapp/src/main/java/org/mycore/frontend/jsp/taglibs/browsing/MCRSearchresultBrowserTag.java
+++ jspdocportal-webapp/src/main/java/org/mycore/frontend/jsp/taglibs/browsing/MCRSearchresultBrowserTag.java
@@ -173,6 +173,7 @@ public class MCRSearchresultBrowserTag extends SimpleTagSupport {
 		}
 		
 	    for(int i=0;i<count;i++){
+	    	out.write(messages.getString("Webpage.searchresults.resort-label"));
 	    	out.write("   <select name=\"field"+Integer.toString(i+1)+"\">");
 	    	for(String fieldname:fieldnames){
 	    		if(fieldname.length()==0){
