a8b44f0eab50fed31e3815e155b2cebb28e27482
Robert Stephan <rsteph-de@users.noreply.github.com>
2011-05-31 20:28:38 +0000

    Formatter for MODS "relatedItem" Element
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@21119 7089d063-bc14-496d-ac90-53de1872eb6c

--- jspdocportal-webapp/src/main/java/org/mycore/frontend/jsp/taglibs/docdetails/helper/MCRPublishedInFormatter.java
+++ jspdocportal-webapp/src/main/java/org/mycore/frontend/jsp/taglibs/docdetails/helper/MCRPublishedInFormatter.java
@@ -23,27 +23,47 @@
  **/
 package org.mycore.frontend.jsp.taglibs.docdetails.helper;
 
+import java.util.List;
+
 import org.jdom.Element;
+import org.mycore.common.MCRConstants;
 
 /**
  * Formats detailed information of published in
- * The element content is described in <publishdetails> Element of document datamodel:
- * 
- * Differenzierende Angaben zur Quelle (Nachbildung von Pica-Kategorie 4070) 
- *     <publishdetail>
- *   	  <title>Titel</title>
- *   	  <v>Band</v><j>Jahr</j><a>Heft</a>
- *   	  <d>Tag</d><m>Monat</m><n>Sonderheft</n>
- *   	  <p>Seitenangabe</p><t>Gesamtzahl Seiten</t>
- *   	  <display>modifizierte Anzeigeform</display>
- *     </publishdetail>
+ * either in mods:relatedItem or mcr:publishdetail element
  *
  * @author Robert Stephan
  * @version $Revision: 17081 $ $Date: 2010-03-20 18:37:27 +0100 (Sa, 20 Mrz 2010) $
  */
 
 public class MCRPublishedInFormatter {
+		
 	public static String format(Element el){
+		if(el.getName().equals("publishdetail")){
+			return formatPublishDetail(el);
+		}
+		if(el.getNamespace().equals(MCRConstants.MODS_NAMESPACE) && el.getName().equals("relatedItem")){
+			return formatRelatedItem(el);
+		}
+		return "";
+		
+	}
+	
+	/**
+	 * @param el the Element containing the content
+	 *  The content is described in <publishdetails> Element of document datamodel:
+	 * 	   <publishdetail>
+	 *   	  <title>Titel</title>
+	 *   	  <v>Band</v><j>Jahr</j><a>Heft</a>
+	 *   	  <d>Tag</d><m>Monat</m><n>Sonderheft</n>
+	 *   	  <p>Seitenangabe</p><t>Gesamtzahl Seiten</t>
+	 *   	  <display>modifizierte Anzeigeform</display>
+	 *     </publishdetail>
+	 * 
+	 * 
+	 * @return a formatted String
+	 */
+	private static String formatPublishDetail(Element el){	
 		StringBuffer result = new StringBuffer();
 		String s = null;
 		s = el.getChildTextNormalize("title");
@@ -142,4 +162,124 @@ public class MCRPublishedInFormatter {
 		}
 		return result.toString();
 	}
+	
+	
+	/**
+	 * @param el the element containing the content
+	 * The content should be a mods:relatedItem Element
+	 * look into the MODS standard for further reference.
+	 * 
+	 * <relatedItem displayLabel="in" type="host">
+     *       <titleInfo>
+     *         <title>Jahrbücher des Vereins für Mecklenburgische Geschichte und Altertumskunde</title>
+     *       </titleInfo>
+     *       <part>
+     *         <detail type="volume">
+     *           <number>1</number>
+     *         </detail>
+     *         <date>1836</date>
+     *         <extend unit="page">
+     *           <start>176</start>
+     *           <end>178</end>
+     *         </extend>
+     *       </part>
+     *     </relatedItem>
+     *
+	 * @return the formatted text
+	 * 
+	 */
+	@SuppressWarnings("unchecked")
+	private static String formatRelatedItem(Element el){
+		StringBuffer sb = new StringBuffer();
+		Element eTitleInfo = el.getChild("titleInfo", MCRConstants.MODS_NAMESPACE);
+		if(eTitleInfo!=null){
+			Element eNonSort = eTitleInfo.getChild("nonSort", MCRConstants.MODS_NAMESPACE);
+			if(eNonSort!=null){
+				sb.append(eNonSort.getText());
+			}
+			Element eTitle = eTitleInfo.getChild("title", MCRConstants.MODS_NAMESPACE);
+			if(eTitle!=null){
+				if(sb.length()>0){
+					sb.append(" ");
+				}
+				sb.append(eTitle.getText());
+			}
+			Element eSubTitle = eTitleInfo.getChild("subTitle", MCRConstants.MODS_NAMESPACE);
+			if(eSubTitle!=null){
+				if(sb.length()>0){
+					sb.append(" : ");
+				}
+				sb.append(eSubTitle.getText());
+			}
+			Element ePart = el.getChild("part", MCRConstants.MODS_NAMESPACE);
+			if(ePart!=null){
+				for(Element eDetail: (List<Element>)(ePart.getChildren("detail", MCRConstants.MODS_NAMESPACE))){
+					if(eDetail.getAttributeValue("type").equals("volume")){
+						if(sb.length()>0){
+							sb.append(", ");
+						}
+						Element eCaption = eDetail.getChild("caption", MCRConstants.MODS_NAMESPACE);
+						if(eCaption!=null){
+							sb.append(eCaption.getText()).append(" ");
+						}
+						else{
+							sb.append("Bd. ");
+						}
+						Element eNumber = eDetail.getChild("number", MCRConstants.MODS_NAMESPACE);
+						
+						if(eNumber!=null){
+							sb.append(eNumber.getText());
+						}
+					}
+					if(eDetail.getAttributeValue("type").equals("issue")){
+						if(sb.length()>0){
+							sb.append(", ");
+						}
+						Element eCaption = eDetail.getChild("caption", MCRConstants.MODS_NAMESPACE);
+						if(eCaption!=null){
+							sb.append(eCaption.getText()).append(" ");
+						}
+						else{
+							sb.append("Nr. ");
+						}
+						Element eNumber = eDetail.getChild("number", MCRConstants.MODS_NAMESPACE);
+						
+						if(eNumber!=null){
+							sb.append(eNumber.getText());
+						}
+					}
+				}
+			
+				Element eDate = ePart.getChild("date", MCRConstants.MODS_NAMESPACE);
+				if(eDate!=null){
+					sb.append(" (").append(eDate.getText()).append(")");
+				}
+				for(Element eExtend: (List<Element>)(ePart.getChildren("extend", MCRConstants.MODS_NAMESPACE))){
+					if(eExtend.getAttributeValue("unit").equals("page")){
+						String s = eExtend.getChildText("list", MCRConstants.MODS_NAMESPACE);
+						if(s!=null){
+							sb.append(", ").append(s);
+						}
+						s = eExtend.getChildText("total", MCRConstants.MODS_NAMESPACE);
+						if(s!=null){
+							sb.append(", ").append(s).append(" Seiten");
+						}
+						s = eExtend.getChildText("start", MCRConstants.MODS_NAMESPACE);
+						if(s!=null){
+							sb.append(", S. ").append(s);
+						}
+						s = eExtend.getChildText("end", MCRConstants.MODS_NAMESPACE);
+						if(s!=null){
+							sb.append("-").append(s);
+						}						
+					}
+				}
+					
+				
+			}
+			
+		}
+		return sb.toString();
+	}
+	
 }
