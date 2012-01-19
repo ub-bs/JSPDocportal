d13d7eb72a4e584b84bd5608856cff98b7889dc8
Robert Stephan <rsteph-de@users.noreply.github.com>
2012-01-19 17:35:43 +0000

    new setLanguage tag which generates the HTML Code for a language changer
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@23233 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ jspdocportal-webapp/src/main/java/org/mycore/frontend/jsp/taglibs/MCRSetLanguageTag.java
@@ -0,0 +1,86 @@
+/*
+ * $RCSfile$
+ * $Revision: 16360 $ $Date: 2010-01-06 00:54:02 +0100 (Mi, 06 Jan 2010) $
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+package org.mycore.frontend.jsp.taglibs;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import org.mycore.common.MCRSession;
+import org.mycore.frontend.servlets.MCRServlet;
+/**
+ * Tag that retrieves the current languages from session
+ * and stores it in a given variable.
+ * 
+ * If the request contains a lang parameter
+ * the given language will be set into session
+ * 
+ * @author Robert Stephan
+ *
+ */
+public class MCRSetLanguageTag extends SimpleTagSupport
+{
+	private String var;
+	private String allowedLanguages="";
+	
+	/**
+	 * Sets the variable name, where the language should be saved to.
+	 * @param inputVar
+	 */
+	public void setVar(String inputVar) {
+		var = inputVar;
+		return;
+	}
+	
+	/**
+	 * Provides a list of comma separated language names (2 letter ISO 639-1 code) 
+	 * which are valid as current language.
+	 * @param allowedLanguages
+	 */
+	public void setAllowedLanguages(String allowedLanguages) {
+		this.allowedLanguages = allowedLanguages;
+	}	
+		
+	public void doTag() throws JspException, IOException {
+		PageContext pageContext = (PageContext) getJspContext();
+		MCRSession mcrSession = MCRServlet.getSession((HttpServletRequest)pageContext.getRequest());
+		
+		String requestParamLang = pageContext.getRequest().getParameter("lang");
+		if(requestParamLang!=null){
+			String[] langArray = allowedLanguages.split(",");
+			List<String> languages = new ArrayList<String>();
+			for(int i=0;i<langArray.length;i++){
+				languages.add(langArray[i].trim());
+			}
+			if(languages.contains(requestParamLang)){
+				mcrSession.setCurrentLanguage(requestParamLang);
+			}		
+		}
+		pageContext.setAttribute(var, mcrSession.getCurrentLanguage());
+	}
+}
