457ca807dcbca3f5d50bb711bc4fbc5dd21d5fa7
Robert Stephan <rsteph-de@users.noreply.github.com>
2013-10-30 15:27:43 +0000

    rest api list objects - ongoing development
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@28289 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ jspdocportal-webapp/src/main/java/org/mycore/frontend/restapi/v1/errors/MCRRestAPIFieldError.java
@@ -0,0 +1,19 @@
+package org.mycore.frontend.restapi.v1.errors;
+
+public class MCRRestAPIFieldError {
+    private String field;
+    private String message;
+    
+    public String getField() {
+        return field;
+    }
+    public void setField(String field) {
+        this.field = field;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
