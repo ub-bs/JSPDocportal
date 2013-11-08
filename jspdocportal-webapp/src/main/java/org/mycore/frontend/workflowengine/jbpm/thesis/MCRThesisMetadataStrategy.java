b1ad3fe2c77918f1be8f30158c06589f14eb4ecf
Robert Stephan <rsteph-de@users.noreply.github.com>
2013-11-08 16:09:38 +0000

    validation of author names for thesis
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@28343 7089d063-bc14-496d-ac90-53de1872eb6c

--- jspdocportal-webapp/src/main/java/org/mycore/frontend/workflowengine/jbpm/thesis/MCRThesisMetadataStrategy.java
+++ jspdocportal-webapp/src/main/java/org/mycore/frontend/workflowengine/jbpm/thesis/MCRThesisMetadataStrategy.java
@@ -3,7 +3,11 @@ import java.util.Iterator;
 
 import org.jbpm.context.exe.ContextInstance;
 import org.jdom2.Element;
+import org.jdom2.Text;
 import org.jdom2.filter.ElementFilter;
+import org.jdom2.filter.Filters;
+import org.jdom2.xpath.XPathExpression;
+import org.jdom2.xpath.XPathFactory;
 import org.mycore.frontend.workflowengine.jbpm.MCRWorkflowConstants;
 import org.mycore.frontend.workflowengine.strategies.MCRDefaultMetadataStrategy;
 
@@ -26,15 +30,19 @@ public class MCRThesisMetadataStrategy extends MCRDefaultMetadataStrategy {
         else{
         	ctxI.setVariable("wfo-title", sbTitle.toString());
         }
-			
-		
-		StringBuffer sbAuthorNames = new StringBuffer("");
-		for(Iterator<Element> it = metadata.getDescendants(new ElementFilter("creator")); it.hasNext();){
-				Element creator = it.next();
-				sbAuthorNames.append(creator.getText());
-				if(it.hasNext()){ sbAuthorNames.append("; ");}
-			}	
-		ctxI.setVariable(MCRWorkflowConstants.WFM_VAR_AUTHOR_NAMES, sbAuthorNames.toString());
+
+        StringBuffer sbAuthorNames = new StringBuffer("");
+        XPathExpression<Text> xpeText = XPathFactory.instance().compile(".//*/text()", Filters.text());
+
+        for(Iterator<Element> it = metadata.getDescendants(new ElementFilter("creator")); it.hasNext();){
+            Element creator = it.next();
+            for(Text t: xpeText.evaluate(creator)){
+                sbAuthorNames.append(t.getTextNormalize()).append(" ");
+            }
+            if(it.hasNext()){ sbAuthorNames.append("; ");}
+        }   
+        
+        ctxI.setVariable(MCRWorkflowConstants.WFM_VAR_AUTHOR_NAMES, sbAuthorNames.toString());
 		
 		StringBuffer sbURN = new StringBuffer("");
 		for(Iterator<Element> it = metadata.getDescendants(new ElementFilter("urn")); it.hasNext();){
