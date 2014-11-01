780669f6f4dae841104c333a8ee06966ea29afde
Robert Stephan <rsteph-de@users.noreply.github.com>
2014-11-01 18:57:00 +0000

    reimplemented feedback form
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@31056 7089d063-bc14-496d-ac90-53de1872eb6c

--- jspdocportal-module/src/main/java/org/mycore/activiti/MCRActivitiMgr.java
+++ jspdocportal-module/src/main/java/org/mycore/activiti/MCRActivitiMgr.java
@@ -1,30 +1,37 @@
 package org.mycore.activiti;
 
+import java.util.Arrays;
+
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.delegate.DelegateExecution;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import org.apache.log4j.Logger;
 import org.mycore.activiti.workflows.create_object_simple.MCRWorkflowMgr;
 import org.mycore.common.MCRException;
 import org.mycore.common.config.MCRConfiguration;
 import org.mycore.common.config.MCRConfigurationException;
 
 public class MCRActivitiMgr {
+	private static final Logger LOGGER = Logger.getLogger(MCRActivitiMgr.class);
 	public static final String WF_VAR_PROJECT_ID = "projectID";
 	public static final String WF_VAR_OBJECT_TYPE = "objectType";
 	public static final String WF_VAR_MCR_OBJECT_ID = "mcrObjectID";
 	public static final String WF_VAR_VALIDATION_RESULT = "validationResult";
 	public static final String WF_VAR_VALIDATION_MESSAGE = "validationMessage";
-	
-	public static final String ACTIVITI_CONFIG_FILE="/config/workflow/activiti.cfg.xml";
+
+	public static final String ACTIVITI_CONFIG_FILE = "/config/workflow/activiti.cfg.xml";
 
 	private static ProcessEngine activitiProcessEngine;
 
 	// Workflow Engine
 	public static synchronized ProcessEngine getWorfklowProcessEngine() {
 		if (activitiProcessEngine == null) {
-			activitiProcessEngine = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource(ACTIVITI_CONFIG_FILE)
-					.buildProcessEngine();
+			ProcessEngineConfiguration pec = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource(ACTIVITI_CONFIG_FILE);
+			activitiProcessEngine = pec.buildProcessEngine();
 		}
 		return activitiProcessEngine;
 	}
@@ -65,5 +72,53 @@ public class MCRActivitiMgr {
 		}
 
 		return mgr;
+	}
+
+	public static SimpleEmail createNewEmailFromConfig() {
+		SimpleEmail email = new SimpleEmail();
+		MCRConfiguration config = MCRConfiguration.instance();
+
+		String host = config.getString("MCR.Workflow.Email.MailServerHost");
+		if (host == null) {
+			LOGGER.error("Email is not configured!");
+			return null;
+		}
+		email.setHostName(host);
+		if (StringUtils.isNotBlank(config.getString("MCR.Workflow.Email.MailServerPort"))) {
+			try {
+				email.setSmtpPort(config.getInt("MCR.Workflow.Email.MailServerPort"));
+			} catch (NumberFormatException nfe) {
+				LOGGER.debug(nfe);
+			}
+		}
+		if (StringUtils.isNotBlank(config.getString("MCR.Workflow.Email.MailServerUseSSL"))) {
+			email.setSSL(config.getBoolean("MCR.Workflow.Email.MailServerUseSSL", false));
+		}
+		if (StringUtils.isNotBlank(config.getString("MCR.Workflow.Email.MailServerUseTLS"))) {
+			email.setTLS(config.getBoolean("MCR.Workflow.Email.MailServerUseTLS", false));
+		}
+		if (StringUtils.isNoneBlank(config.getString("MCR.Workflow.Email.MailServerUsername"), config.getString("MCR.Workflow.Email.MailServerPassword"))) {
+			email.setAuthentication(config.getString("MCR.Workflow.Email.MailServerUsername"), config.getString("MCR.Workflow.Email.MailServerPassword"));
+		}
+		if (StringUtils.isNotBlank(config.getString("MCR.Workflow.Email.MailServerUseTLS"))) {
+			email.setTLS(config.getBoolean("MCR.Workflow.Email.MailServerUseTLS", false));
+		}
+
+		try {
+			if (StringUtils.isNotBlank(config.getString("MCR.Workflow.Email.From"))) {
+				email.setFrom(config.getString("MCR.Workflow.Email.From"));
+			}
+
+			if (StringUtils.isNoneBlank(config.getString("MCR.Workflow.Email.From"), config.getString("MCR.Workflow.Email.Sender"))) {
+				email.setFrom(config.getString("MCR.Workflow.Email.From"), config.getString("MCR.Workflow.Email.Sender"));
+			}
+			if (StringUtils.isNotBlank(config.getString("MCR.Workflow.Email.CC"))) {
+				email.setCc(Arrays.asList(config.getString("MCR.Workflow.Email.CC").split(",")));
+			}
+		} catch (EmailException e) {
+			LOGGER.error(e);
+		}
+
+		return email;
 	}
 }
---
+++ jspdocportal-module/src/main/java/org/mycore/frontend/jsp/stripes/actions/SendFeedbackAction.java
@@ -0,0 +1,232 @@
+package org.mycore.frontend.jsp.stripes.actions;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.Before;
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.RedirectResolution;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.SimpleMessage;
+import net.sourceforge.stripes.action.UrlBinding;
+import net.sourceforge.stripes.controller.LifecycleStage;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import org.apache.log4j.Logger;
+import org.mycore.activiti.MCRActivitiMgr;
+import org.mycore.common.config.MCRConfiguration;
+import org.mycore.services.i18n.MCRTranslation;
+
+
+@UrlBinding("/feedback.action")
+public class SendFeedbackAction extends MCRAbstractStripesAction implements ActionBean {
+	private static Logger LOGGER = Logger.getLogger(SendFeedbackAction.class);
+	
+	private static final Pattern EMAIL_PATTERN = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+	
+	ForwardResolution fwdResolution = new ForwardResolution("/content/feedback.jsp");
+	
+	private String fromName;
+	private String fromEmail;
+	private String topicURL;
+	private String topicHeader;
+	private String message;
+	private String recipient;
+	private String subject;
+	private String returnURL;
+	
+	public SendFeedbackAction() {
+		recipient = MCRConfiguration.instance().getString("MCRWorkflow.Email.Feedback.Recipient", "");
+	}
+
+	@Before(stages = LifecycleStage.BindingAndValidation)
+	public void rehydrate() {
+		super.rehydrate();
+		if (getContext().getRequest().getParameter("message") != null) {
+			message = getContext().getRequest().getParameter("message");
+		}
+		if (getContext().getRequest().getParameter("topicURL") != null) {
+			topicURL = getContext().getRequest().getParameter("topicURL");
+		}
+		if (getContext().getRequest().getParameter("topicHeader") != null) {
+			topicHeader = getContext().getRequest().getParameter("topicHeader");
+		}
+		if (getContext().getRequest().getParameter("fromName") != null) {
+			fromName = getContext().getRequest().getParameter("fromName");
+		}
+		if (getContext().getRequest().getParameter("fromEmail") != null) {
+			fromEmail = getContext().getRequest().getParameter("fromEmail");
+		}
+		if (getContext().getRequest().getParameter("recipient") != null) {
+			recipient = getContext().getRequest().getParameter("recipient");
+		}
+		if (getContext().getRequest().getParameter("subject") != null) {
+			subject = getContext().getRequest().getParameter("subject");
+		}
+		if (getContext().getRequest().getParameter("returnURL") != null) {
+			returnURL = getContext().getRequest().getParameter("returnURL");
+		}
+	}
+
+	@DefaultHandler
+	public Resolution defaultRes() {
+		if(StringUtils.isBlank(returnURL)){
+			returnURL = getContext().getRequest().getHeader("Referer");
+		}
+		return fwdResolution;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Resolution doSend() {
+		SimpleEmail email = MCRActivitiMgr.createNewEmailFromConfig();
+		try{
+			if(StringUtils.isNotBlank(message)){
+				email.setMsg(message);
+			}
+			else{
+				getContext().getMessages().add(new SimpleMessage(MCRTranslation.translate("WF.messages.feedback.noMessage")));
+			}
+			boolean isEmailValid = true;
+			if(StringUtils.isNotBlank(fromEmail)){
+				isEmailValid = EMAIL_PATTERN.matcher(fromEmail).matches();
+				if(!isEmailValid){
+					getContext().getMessages().add(new SimpleMessage(MCRTranslation.translate("WF.messages.feedback.wrongEmailFormat")));
+				}
+			}
+			
+			if(StringUtils.isNotBlank(fromName)){
+				if(StringUtils.isNotBlank(fromEmail)){
+					email.setCc(Arrays.asList(new InternetAddress[]{new InternetAddress(fromEmail, fromName)}));
+					email.setReplyTo(Arrays.asList(new InternetAddress[]{new InternetAddress(fromEmail)}));
+				}
+			}
+			else{
+				getContext().getMessages().add(new SimpleMessage(MCRTranslation.translate("WF.messages.feedback.noName")));
+			}
+			
+			if(StringUtils.isNoneBlank(message, fromName) && isEmailValid){
+				MCRConfiguration config = MCRConfiguration.instance();
+				
+				StringBuilder sbMsg = new StringBuilder();
+				sbMsg.append(config.getString("MCRWorkflow.Email.Feedback.Subject","Feedbackformular"));
+				sbMsg.append("\n\n*Angaben zu:*");
+				sbMsg.append("\n"+topicHeader);
+				sbMsg.append("\n("+topicURL+")");
+				sbMsg.append("\n\n*Absender:*");
+				sbMsg.append("\n"+fromName);
+				if(StringUtils.isNotBlank(fromEmail)){
+					sbMsg.append(" ("+fromEmail+")");
+				}
+				sbMsg.append("\n\n*Nachricht:*");
+				sbMsg.append("\n"+message);
+				
+				String recipient = config.getString("MCRWorkflow.Email.Feedback.Recipient");
+				email.getToAddresses().add(new InternetAddress(recipient));
+				String[] cc = config.getString("MCR.Workflow.Email.CC", "").split(","); 
+				for(String s: cc){
+					s = s.trim();
+					if(StringUtils.isNotBlank(s)){
+						email.getCcAddresses().add(new InternetAddress(s.trim()));
+					}
+				}
+				if(StringUtils.isNotBlank(fromEmail)){
+					email.getCcAddresses().add(new InternetAddress(fromEmail));
+				}
+				email.setMsg(sbMsg.toString());
+				email.setSubject(config.getString("MCRWorkflow.Email.Feedback.Subject","Feedbackformular"));
+				
+				email.send();
+				
+				if(StringUtils.isNotBlank(returnURL)){
+					return new RedirectResolution(returnURL, false);
+				}
+			}
+		}
+		catch(EmailException e){
+			LOGGER.error(e);
+			getContext().getMessages().add(new SimpleMessage(e.getMessage()));
+			
+		}
+		catch(AddressException e){
+			LOGGER.error(e);
+			getContext().getMessages().add(new SimpleMessage(e.getMessage()));
+		}
+		catch(UnsupportedEncodingException e){
+			
+		}
+		return fwdResolution;
+	}
+
+	public String getFromName() {
+		return fromName;
+	}
+
+	public void setFromName(String fromName) {
+		this.fromName = fromName;
+	}
+
+	public String getFromEmail() {
+		return fromEmail;
+	}
+
+	public void setFromEmail(String fromEmail) {
+		this.fromEmail = fromEmail;
+	}
+
+	public String getTopicURL() {
+		return topicURL;
+	}
+
+	public void setTopicURL(String topicURL) {
+		this.topicURL = topicURL;
+	}
+
+	public String getTopicHeader() {
+		return topicHeader;
+	}
+
+	public void setTopicHeader(String topicHeader) {
+		this.topicHeader = topicHeader;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getRecipient() {
+		return recipient;
+	}
+
+	public void setRecipient(String recipient) {
+		this.recipient = recipient;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getReturnURL() {
+		return returnURL;
+	}
+
+	public void setReturnURL(String returnURL) {
+		this.returnURL = returnURL;
+	}
+}
