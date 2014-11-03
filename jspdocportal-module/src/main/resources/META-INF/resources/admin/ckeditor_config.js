b2819e9072f04dd84e9d1b65e55ea0aaa0059ff3
Robert Stephan <rsteph-de@users.noreply.github.com>
2014-11-03 00:25:34 +0000

    editwebcontent tag and action, integration of ck editor
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@31060 7089d063-bc14-496d-ac90-53de1872eb6c

--- jspdocportal-module/src/main/resources/META-INF/resources/admin/ckeditor_config.js
+++ jspdocportal-module/src/main/resources/META-INF/resources/admin/ckeditor_config.js
@@ -4,7 +4,7 @@ CKEDITOR.editorConfig = function( config )
     config.uiColor = '#80C5DD';
     config.height = '450px';
     config.toolbar = 'Full'; 
-
+/*
     config.toolbar_Full =
     [
         ['Source','-','Preview','-','Templates'],
@@ -22,5 +22,99 @@ CKEDITOR.editorConfig = function( config )
         ['TextColor','BGColor'],
         ['Maximize', 'ShowBlocks','-','About']
     ];
+  */  
+	config.contentsCss = 'assets/output_xhtml.css';
 
+	/*
+	 * Core styles.
+	 */
+	config.coreStyles_bold	= { element : 'span', attributes : {'class': 'Bold'} };
+	config.coreStyles_italic	= { element : 'span', attributes : {'class': 'Italic'}};
+	config.coreStyles_underline	= { element : 'span', attributes : {'class': 'Underline'}};
+	config.coreStyles_strike	= { element : 'span', attributes : {'class': 'StrikeThrough'}, overrides : 'strike' };
+
+	config.coreStyles_subscript = { element : 'span', attributes : {'class': 'Subscript'}, overrides : 'sub' };
+	config.coreStyles_superscript = { element : 'span', attributes : {'class': 'Superscript'}, overrides : 'sup' };
+
+	/*
+	 * Font face
+	 */
+	// List of fonts available in the toolbar combo. Each font definition is
+	// separated by a semi-colon (;). We are using class names here, so each font
+	// is defined by {Combo Label}/{Class Name}.
+	config.font_names = 'Comic Sans MS/FontComic;Courier New/FontCourier;Times New Roman/FontTimes';
+
+	// Define the way font elements will be applied to the document. The "span"
+	// element will be used. When a font is selected, the font name defined in the
+	// above list is passed to this definition with the name "Font", being it
+	// injected in the "class" attribute.
+	// We must also instruct the editor to replace span elements that are used to
+	// set the font (Overrides).
+	config.font_style =
+	{
+			element		: 'span',
+			attributes		: { 'class' : '#(family)' },
+			overrides	: [ { element : 'span', attributes : { 'class' : /^Font(?:Comic|Courier|Times)$/ } } ]
+	};
+
+	/*
+	 * Font sizes.
+	 */
+	config.fontSize_sizes = 'Smaller/FontSmaller;Larger/FontLarger;8pt/FontSmall;14pt/FontBig;Double Size/FontDouble';
+	config.fontSize_style =
+		{
+			element		: 'span',
+			attributes	: { 'class' : '#(size)' },
+			overrides	: [ { element : 'span', attributes : { 'class' : /^Font(?:Smaller|Larger|Small|Big|Double)$/ } } ]
+		};
+
+	/*
+	 * Font colors.
+	 */
+	config.colorButton_enableMore = false;
+
+	config.colorButton_colors = 'FontColor1/FF9900,FontColor2/0066CC,FontColor3/F00';
+	config.colorButton_foreStyle =
+		{
+			element : 'span',
+			attributes : { 'class' : '#(color)' },
+			overrides	: [ { element : 'span', attributes : { 'class' : /^FontColor(?:1|2|3)$/ } } ]
+		};
+
+	config.colorButton_backStyle =
+		{
+			element : 'span',
+			attributes : { 'class' : '#(color)BG' },
+			overrides	: [ { element : 'span', attributes : { 'class' : /^FontColor(?:1|2|3)BG$/ } } ]
+		};
+
+	/*
+	 * Indentation.
+	 */
+	config.indentClasses = ['Indent1', 'Indent2', 'Indent3'];
+
+	/*
+	 * Paragraph justification.
+	 */
+	config.justifyClasses = [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyFull' ];
+
+	/*
+	 * Styles combo.
+	 */
+	config.stylesSet =
+			[
+				{ name : 'Strong Emphasis', element : 'strong' },
+				{ name : 'Emphasis', element : 'em' },
+
+				{ name : 'Computer Code', element : 'code' },
+				{ name : 'Keyboard Phrase', element : 'kbd' },
+				{ name : 'Sample Text', element : 'samp' },
+				{ name : 'Variable', element : 'var' },
+
+				{ name : 'Deleted Text', element : 'del' },
+				{ name : 'Inserted Text', element : 'ins' },
+
+				{ name : 'Cited Work', element : 'cite' },
+				{ name : 'Inline Quotation', element : 'q' }
+			];
 };
