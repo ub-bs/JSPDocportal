c25cd1f4ededb9ed06fc868c5e051a315f2d89db
Robert Stephan <rsteph-de@users.noreply.github.com>
2016-05-31 15:08:53 +0000

    enhanced normalizer für accented characters
    git-svn-id: https://server.mycore.de/svn/jspdocportal/branches/mycore-2015.05.0.x@35540 7089d063-bc14-496d-ac90-53de1872eb6c

--- jspdocportal-module/src/main/java/org/mycore/frontend/jsp/MCRJSPUtils.java
+++ jspdocportal-module/src/main/java/org/mycore/frontend/jsp/MCRJSPUtils.java
@@ -1,9 +1,13 @@
 package org.mycore.frontend.jsp;
 
+import java.text.Normalizer;
+
 public class MCRJSPUtils {
     public static String normalizeUmlauts(String input){
         String result = input.replace("ä",  "ae").replace("ö", "oe").replace("ü", "ue").replace("ß", "ss");
         result = result.replace("Ä",  "AE").replace("Ö",  "OE").replace("Ü",  "UE");
+        result = Normalizer.normalize(result, Normalizer.Form.NFD);
+        result = result.replaceAll("[^\\p{ASCII}]", "");
         return result;
     }
 }
