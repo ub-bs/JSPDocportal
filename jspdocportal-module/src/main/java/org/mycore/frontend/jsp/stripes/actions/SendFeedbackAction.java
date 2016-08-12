48aa72c52baeade724a22504a8b9247fb349f369
Robert Stephan <rsteph-de@users.noreply.github.com>
2016-08-12 17:29:07 +0000

    improved message body of feedback action
    git-svn-id: https://server.mycore.de/svn/jspdocportal/branches/mycore-2015.05.0.x@36137 7089d063-bc14-496d-ac90-53de1872eb6c

--- jspdocportal-module/src/main/java/org/mycore/frontend/jsp/stripes/actions/SendFeedbackAction.java
+++ jspdocportal-module/src/main/java/org/mycore/frontend/jsp/stripes/actions/SendFeedbackAction.java
@@ -120,15 +120,22 @@ public class SendFeedbackAction extends MCRAbstractStripesAction implements Acti
 				subject = subject.replace("{0}",  topicHeader);
 				StringBuilder sbMsg = new StringBuilder();
 				sbMsg.append(subject);
-				sbMsg.append("\n\n*Angaben zu:*");
+				sbMsg.append("\n"+StringUtils.repeat("=", subject.length()));
+				sbMsg.append("\n");
+				sbMsg.append("\nAngaben zu:");
+				sbMsg.append("\n-----------");
 				sbMsg.append("\n"+topicHeader);
 				sbMsg.append("\n("+topicURL+")");
-				sbMsg.append("\n\n*Absender:*");
+				sbMsg.append("\n");
+				sbMsg.append("\nAbsender:");
+				sbMsg.append("\n---------");
 				sbMsg.append("\n"+fromName);
 				if(StringUtils.isNotBlank(fromEmail)){
 					sbMsg.append(" ("+fromEmail+")");
 				}
-				sbMsg.append("\n\n*Nachricht:*");
+				sbMsg.append("\n");
+				sbMsg.append("\nNachricht:");
+				sbMsg.append("\n----------");
 				sbMsg.append("\n"+message);
 				
 				String recipient = config.getString("MCRWorkflow.Email.Feedback.Recipient");
