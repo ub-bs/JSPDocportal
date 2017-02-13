bff1fb5c187bd30ebb98031e09538b339e72b034
Robert Stephan <rsteph-de@users.noreply.github.com>
2017-02-13 09:54:36 +0000

    add javascript to editor that checks gnds at  lobid.org and displays the found name as title tag
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@36968 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ jspdocportal-module/src/main/resources/META-INF/resources/javascript/xed-check-gnd.js
@@ -0,0 +1,20 @@
+/**
+ * checks the gnd via lobid.org used in xeditor
+ */
+
+function checkGND(self) {
+	var gnd = $(self).val().replace('http://d-nb.info/gnd/', '');
+	if (gnd.length > 0) {
+		$.ajax({
+			type : 'GET',
+			url : 'https://lobid.org/person?format=short.preferredNameForThePerson&id='+ gnd,
+			cache : false,
+			dataType: 'jsonp',
+			success : function(data) {
+				if ($(data).size() > 1) {
+					$(self).attr('title', 'Name in GND (via lobid.org): '+data[1]);
+				}
+			}
+		});
+	}
+}
