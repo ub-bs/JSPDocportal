4d2475800b3d4d0d216e44b1d8ad0a9ac6fb6f71
Robert Stephan <rsteph-de@users.noreply.github.com>
2017-08-18 11:21:31 +0000

    JDP-28 #comment remove java code
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@37248 7089d063-bc14-496d-ac90-53de1872eb6c

--- jspdocportal-module/src/main/java/org/mycore/frontend/workflowengine/strategies/MCRBackupStrategy.java
+++
@@ -1,9 +0,0 @@
-package org.mycore.frontend.workflowengine.strategies;
-
-import org.jbpm.context.exe.ContextInstance;
-
-public interface MCRBackupStrategy {
-		
-	public boolean backupWorkflowFiles(ContextInstance ctxI);
-
-}
