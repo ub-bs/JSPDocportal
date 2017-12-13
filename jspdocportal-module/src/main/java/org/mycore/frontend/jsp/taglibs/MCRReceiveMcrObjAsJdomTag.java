2f8c81d94db08c890025e0959a1bea005db25d08
Robert Stephan <rsteph-de@users.noreply.github.com>
2017-12-13 14:56:12 +0000

    use cache for mycore object
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@37495 7089d063-bc14-496d-ac90-53de1872eb6c

--- jspdocportal-module/src/main/java/org/mycore/frontend/jsp/taglibs/MCRReceiveMcrObjAsJdomTag.java
+++ jspdocportal-module/src/main/java/org/mycore/frontend/jsp/taglibs/MCRReceiveMcrObjAsJdomTag.java
@@ -1,6 +1,7 @@
 package org.mycore.frontend.jsp.taglibs;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
@@ -8,14 +9,33 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jdom2.Document;
 import org.jdom2.output.DOMOutputter;
 import org.mycore.activiti.MCRActivitiUtils;
 import org.mycore.datamodel.metadata.MCRMetadataManager;
+import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.frontend.jsp.MCRHibernateTransactionWrapper;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
 public class MCRReceiveMcrObjAsJdomTag extends SimpleTagSupport {
     private static Logger logger = LogManager.getLogger(MCRReceiveMcrObjAsJdomTag.class);
+
+    private static DOMOutputter domOutputter = new DOMOutputter();
+
+    private static LoadingCache<String, Document> mcrObjXMLCache = CacheBuilder.newBuilder().maximumSize(300)
+            .expireAfterWrite(3, TimeUnit.MINUTES).expireAfterAccess(15, TimeUnit.SECONDS).build(new CacheLoader<String, Document>() {
+                @Override
+                public Document load(String mcrid) throws Exception {
+                    try (MCRHibernateTransactionWrapper htw = new MCRHibernateTransactionWrapper()) {
+                        MCRObject mcrObj = MCRMetadataManager.retrieveMCRObject(MCRObjectID.getInstance(mcrid));
+                        return mcrObj.createXML();
+                    }
+                }
+            });
 
     private String mcrid;
     private String var;
@@ -39,25 +59,28 @@ public class MCRReceiveMcrObjAsJdomTag extends SimpleTagSupport {
     }
 
     public void doTag() throws JspException, IOException {
-        try (MCRHibernateTransactionWrapper htw = new MCRHibernateTransactionWrapper()) {
-            org.mycore.datamodel.metadata.MCRObject mcr_obj = null;
+        PageContext pageContext = (PageContext) getJspContext();
+        try {
+            Document docJdom = null;
             if (fromWF) {
-                mcr_obj = MCRActivitiUtils.getWorkflowObject(MCRObjectID.getInstance(mcrid));
+                MCRObject mcrObj = MCRActivitiUtils.getWorkflowObject(MCRObjectID.getInstance(mcrid));
+                docJdom = mcrObj.createXML();
             } else {
-                mcr_obj = MCRMetadataManager.retrieveMCRObject(MCRObjectID.getInstance(mcrid));
+                if("clear".equals(pageContext.getRequest().getParameter("_cache"))){
+                    mcrObjXMLCache.invalidate(mcrid);
+                }
+                
+                docJdom = mcrObjXMLCache.get(mcrid);
             }
-            PageContext pageContext = (PageContext) getJspContext();
-            org.jdom2.Document docJdom = mcr_obj.createXML();
             if (var != null && !var.equals("")) {
                 pageContext.setAttribute(var, docJdom);
             }
             if (varDom != null && !varDom.equals("")) {
-                org.w3c.dom.Document domDoc = null;
-                domDoc = new DOMOutputter().output(mcr_obj.createXML());
+                org.w3c.dom.Document domDoc = domOutputter.output(docJdom);
                 pageContext.setAttribute(varDom, domDoc);
             }
         } catch (Exception e) {
-            logger.error("error in receiving mcr_obj as jdom and dom for " + mcrid, e);
+            logger.error("error in receiving mcr_obj as jdom or dom for " + mcrid, e);
         }
     }
 }
