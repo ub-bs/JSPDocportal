78110af95e8b4076ab478ca730750b1a71e4575a
Robert Stephan <rsteph-de@users.noreply.github.com>
2018-05-17 17:00:15 +0000

    JDP-26 #comment fix hasAccessTag (also check for guest user)
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@37651 7089d063-bc14-496d-ac90-53de1872eb6c

--- jspdocportal-module/src/main/java/org/mycore/frontend/jsp/taglibs/MCRHasAccessTag.java
+++ jspdocportal-module/src/main/java/org/mycore/frontend/jsp/taglibs/MCRHasAccessTag.java
@@ -2,7 +2,6 @@ package org.mycore.frontend.jsp.taglibs;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
@@ -10,9 +9,7 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.mycore.access.MCRAccessManager;
-import org.mycore.common.MCRSession;
 import org.mycore.frontend.jsp.MCRHibernateTransactionWrapper;
-import org.mycore.frontend.servlets.MCRServlet;
 
 public class MCRHasAccessTag extends SimpleTagSupport {
     private String permission;
@@ -37,12 +34,8 @@ public class MCRHasAccessTag extends SimpleTagSupport {
     public void doTag() throws JspException, IOException {
         try (MCRHibernateTransactionWrapper htw = new MCRHibernateTransactionWrapper()) {
             PageContext pageContext = (PageContext) getJspContext();
-            MCRSession mcrSession = MCRServlet.getSession((HttpServletRequest) pageContext.getRequest());
-
-            String userID = mcrSession.getUserInformation().getUserID();
-            if ("guest".equals(userID)) {
-                pageContext.setAttribute(var, new Boolean(false));
-            } else if (mcrid == null || "".equals(mcrid)) { // allgemeiner check des aktuellen Users
+            
+            if (mcrid == null || "".equals(mcrid)) { // allgemeiner check des aktuellen Users
                 pageContext.setAttribute(var, new Boolean(MCRAccessManager.checkPermission(permission)));
             } else {
                 pageContext.setAttribute(var, new Boolean(MCRAccessManager.checkPermission(mcrid, permission)));
