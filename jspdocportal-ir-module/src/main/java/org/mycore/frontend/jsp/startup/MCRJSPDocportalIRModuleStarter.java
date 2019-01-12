d50f44fa30f6685d4e5e0de75b1fc7cc7ff6e788
Robert Stephan <rsteph-de@users.noreply.github.com>
2019-01-12 21:20:18 +0000

    implement MyCoRe Starter to disable Tiling
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@37926 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ jspdocportal-ir-module/src/main/java/org/mycore/frontend/jsp/startup/MCRJSPDocportalIRModuleStarter.java
@@ -0,0 +1,33 @@
+package org.mycore.frontend.jsp.startup;
+
+import javax.servlet.ServletContext;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.mycore.common.events.MCRStartupHandler;
+import org.mycore.iview2.services.MCRImageTiler;
+
+
+public class MCRJSPDocportalIRModuleStarter  implements MCRStartupHandler.AutoExecutable {
+    private static Logger LOGGER = LogManager.getLogger(MCRJSPDocportalIRModuleStarter.class);
+
+    @Override
+    public String getName() {
+        return "JSPDocportal IR Module Starter";
+    }
+
+    @Override
+    public int getPriority() {
+        return 0;
+    }
+
+    @Override
+    public void startUp(ServletContext servletContext) {
+        if (servletContext != null && MCRImageTiler.isRunning()) {
+            LOGGER.info("JSPDocportal is going to shutdown Image Viewer Tiling Thread.");
+            MCRImageTiler.getInstance().close();
+        }
+    }
+
+
+}
