d089219535734e7023077975c3682464bcdb84ef
Robert Stephan <rsteph-de@users.noreply.github.com>
2019-01-17 18:59:19 +0000

    fix deprecation warning for StringEscapeUtils (now in commons-text)
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@37946 7089d063-bc14-496d-ac90-53de1872eb6c

--- jspdocportal-module/src/main/java/org/mycore/frontend/jsp/taglibs/docdetails/MCRDocDetailsItemTag.java
+++ jspdocportal-module/src/main/java/org/mycore/frontend/jsp/taglibs/docdetails/MCRDocDetailsItemTag.java
@@ -31,7 +31,7 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.taglibs.standard.tag.common.xml.XPathUtil;
