1e419cf0081e274af555f63175ae7d47c207df14
Robert Stephan <rsteph-de@users.noreply.github.com>
2019-05-06 14:44:23 +0000

    improve calls of MODSGVKImport
    git-svn-id: https://server.mycore.de/svn/jspdocportal/trunk@38046 7089d063-bc14-496d-ac90-53de1872eb6c

--- jspdocportal-module/src/main/java/org/mycore/frontend/jsp/stripes/actions/util/MCRMODSCatalogService.java
+++ jspdocportal-module/src/main/java/org/mycore/frontend/jsp/stripes/actions/util/MCRMODSCatalogService.java
@@ -1,7 +1,11 @@
 package org.mycore.frontend.jsp.stripes.actions.util;
 
+import java.nio.file.Path;
+
+import org.jdom2.Document;
 import org.jdom2.Element;
 
 public interface MCRMODSCatalogService {
+    public void updateWorkflowFile(Path mcrFile, Document docJdom);
     public Element retrieveMODSFromCatalogue(String sruQuery);
 }
