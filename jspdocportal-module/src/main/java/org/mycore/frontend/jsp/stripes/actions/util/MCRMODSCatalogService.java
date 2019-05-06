efa4a657dc6d252deef5f0aa32ddc4e739296bd9
Robert Stephan <rsteph-de@users.noreply.github.com>
2019-05-06 13:42:17 +0000

    use external pica2mods
    git-svn-id: https://server.mycore.de/svn/jspdocportal/branches/mycore-2017.06.0.x@38041 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ jspdocportal-module/src/main/java/org/mycore/frontend/jsp/stripes/actions/util/MCRMODSCatalogService.java
@@ -0,0 +1,7 @@
+package org.mycore.frontend.jsp.stripes.actions.util;
+
+import org.jdom2.Element;
+
+public interface MCRMODSCatalogService {
+    public Element retrieveMODSFromCatalogue(String sruQuery);
+}
--- jspdocportal-module/src/main/java/org/mycore/frontend/jsp/stripes/actions/util/MCRMODSGVKImporter.java
+++ jspdocportal-module/src/main/java/org/mycore/frontend/jsp/stripes/actions/util/MCRMODSGVKImporter.java
@@ -1,23 +1,15 @@
 package org.mycore.frontend.jsp.stripes.actions.util;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jdom2.Document;
 import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.Namespace;
 import org.jdom2.filter.Filters;
-import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 import org.jdom2.xpath.XPathExpression;
@@ -25,209 +17,101 @@ import org.jdom2.xpath.XPathFactory;
 import org.mycore.activiti.MCRActivitiUtils;
 import org.mycore.common.MCRConstants;
 import org.mycore.common.config.MCRConfiguration;
-import org.mycore.common.content.MCRContent;
-import org.mycore.common.content.MCRJDOMContent;
-import org.mycore.common.content.transformer.MCRXSLTransformer;
 import org.mycore.datamodel.metadata.MCRObjectID;
 
 public class MCRMODSGVKImporter {
     private static Logger LOGGER = LogManager.getLogger(MCRMODSGVKImporter.class);
 
-    private static XPathExpression<Element> XP_URN = XPathFactory.instance().compile("//mods:mods/mods:identifier[@type='urn']",
-            Filters.element(), null, MCRConstants.MODS_NAMESPACE);
-    
-    private static XPathExpression<Element> XP_PPN = XPathFactory.instance().compile("//mods:mods/mods:identifier[@type='PPN']",
-            Filters.element(), null, MCRConstants.MODS_NAMESPACE);
-    
-    private static XPathExpression<Element> XP_RECORD_ID = XPathFactory.instance().compile("//mods:mods/mods:recordInfo/mods:recordIdentifier",
-            Filters.element(), null, MCRConstants.MODS_NAMESPACE);
+    private static MCRMODSCatalogService modsCatService = MCRConfiguration.instance()
+        .getInstanceOf("MCR.Workflow.MODSCatalogueService.class");
+
+    private static XPathExpression<Element> XP_URN = XPathFactory.instance().compile(
+        "//mods:mods/mods:identifier[@type='urn']",
+        Filters.element(), null, MCRConstants.MODS_NAMESPACE);
+
+    private static XPathExpression<Element> XP_PPN = XPathFactory.instance().compile(
+        "//mods:mods/mods:identifier[@type='PPN']",
+        Filters.element(), null, MCRConstants.MODS_NAMESPACE);
+
+    private static XPathExpression<Element> XP_RECORD_ID = XPathFactory.instance().compile(
+        "//mods:mods/mods:recordInfo/mods:recordIdentifier",
+        Filters.element(), null, MCRConstants.MODS_NAMESPACE);
 
     private static XPathExpression<Element> XP_MODS_ROOT = XPathFactory.instance().compile("//*[./mods:mods]",
-            Filters.element(), null, MCRConstants.MODS_NAMESPACE);
+        Filters.element(), null, MCRConstants.MODS_NAMESPACE);
 
     public static void updateWorkflowFile(MCRObjectID mcrObjID) {
         Path mcrFile = MCRActivitiUtils.getWorkflowObjectFile(mcrObjID);
         try {
             Document docJdom = MCRActivitiUtils.getWorkflowObjectXML(mcrObjID);
             Element eModsContainer = XP_MODS_ROOT.evaluateFirst(docJdom);
+
             Element eURN = XP_URN.evaluateFirst(docJdom);
             if (eModsContainer != null && eURN != null) {
-                Element ePica = retrievePicaXMLByURN(eURN.getTextTrim());
-                if (ePica != null) {
-                    Element eMODS = transformPica2MODS(ePica);
-                    if (eMODS != null) {
-                        eModsContainer.removeContent();
-                        eModsContainer.addContent(eMODS.detach());
-                        XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
-                        try (BufferedWriter bw = Files.newBufferedWriter(mcrFile)) {
-                            outputter.output(docJdom, bw);
-                        }
-                        return;
-                    }
+                Element eMODS = retrieveMODSByURN(eURN.getTextTrim());
+                if (eMODS != null) {
+                    updateWorkflowMetadataFile(mcrFile, docJdom, eModsContainer, eMODS);
+                    return;
                 }
-
             }
-            
+
             Element eRecordInfo = XP_RECORD_ID.evaluateFirst(docJdom);
             if (eModsContainer != null && eRecordInfo != null) {
-                Element ePica = retrievePicaXMLByPURL(eRecordInfo.getTextTrim());
-                if (ePica != null) {
-                    Element eMODS = transformPica2MODS(ePica);
-                    if (eMODS != null) {
-                        eModsContainer.removeContent();
-                        eModsContainer.addContent(eMODS.detach());
-                        XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
-                        try (BufferedWriter bw = Files.newBufferedWriter(mcrFile)) {
-                            outputter.output(docJdom, bw);
-                        }
-                        return;
-                    }
-                    
+                Element eMODS = retrieveMODSByPURL(eRecordInfo.getTextTrim());
+                if (eMODS != null) {
+                    updateWorkflowMetadataFile(mcrFile, docJdom, eModsContainer, eMODS);
+                    return;
                 }
             }
-            
+
             Element ePPN = XP_PPN.evaluateFirst(docJdom);
             if (eModsContainer != null && ePPN != null) {
-                Element ePica = retrievePicaXMLByPPN(ePPN.getTextTrim());
-                if (ePica != null) {
-                    Element eMODS = transformPica2MODS(ePica);
-                    if (eMODS != null) {
-                        eModsContainer.removeContent();
-                        eModsContainer.addContent(eMODS.detach());
-                        XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
-                        try (BufferedWriter bw = Files.newBufferedWriter(mcrFile)) {
-                            outputter.output(docJdom, bw);
-                        }
-                        return;
-                    }
-                    
+                Element eMODS = retrieveMODSByPPN(ePPN.getTextTrim());
+                if (eMODS != null) {
+                    updateWorkflowMetadataFile(mcrFile, docJdom, eModsContainer, eMODS);
+                    return;
                 }
             }
-            
-            if(eModsContainer!=null) {
-            	Element ePica = retrievePicaXMLByMyCoReID(mcrObjID.toString());
-                if (ePica != null) {
-                    Element eMODS = transformPica2MODS(ePica);
-                    if (eMODS != null) {
-                        eModsContainer.removeContent();
-                        eModsContainer.addContent(eMODS.detach());
-                        XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
-                        try (BufferedWriter bw = Files.newBufferedWriter(mcrFile)) {
-                            outputter.output(docJdom, bw);
-                        }
-                        return;
-                    }
-                }           	
+
+            if (eModsContainer != null) {
+                Element eMODS = retrieveMODSByMyCoReID(mcrObjID.toString());
+                if (eMODS != null) {
+                    updateWorkflowMetadataFile(mcrFile, docJdom, eModsContainer, eMODS);
+                    return;
+                }
             }
-            
         } catch (IOException e) {
             LOGGER.error(e);
         }
-
     }
 
-    private static Element transformPica2MODS(Element ePica) {
-        MCRContent modsResult;
-        try {
-            MCRXSLTransformer transformer = new MCRXSLTransformer(MCRConfiguration.instance().getString("MCR.Editor.Pica2MODS.xslt"));
-           // Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            modsResult = transformer.transform(new MCRJDOMContent(ePica));
-            return modsResult.asXML().getRootElement();
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static void updateWorkflowMetadataFile(Path mcrFile, Document docJdom, Element eModsContainer, Element eMODS)
+        throws IOException {
+        eModsContainer.removeContent();
+        eModsContainer.addContent(eMODS.detach());
+        XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
+        try (BufferedWriter bw = Files.newBufferedWriter(mcrFile)) {
+            outputter.output(docJdom, bw);
         }
-        return null;
     }
 
-    private static Element retrievePicaXMLByURN(String urn) {
-        Element e = null;
-        try {
-            String sruBaseURL = MCRConfiguration.instance().getString("MCR.Editor.Pica2MODS.sru-url");
-            URL urlSRU = new URL(sruBaseURL + "&recordSchema=picaxml&query=pica.urn%3D" + urn);
-            BufferedReader br = new BufferedReader(new InputStreamReader(urlSRU.openStream(), "UTF-8"));
-
-            Document docJdom = new SAXBuilder().build(br, "UTF-8");
-            br.close();
-
-            /*<zs:searchRetrieveResponse xmlns:zs="http://www.loc.gov/zing/srw/">
-                <zs:version>1.1</zs:version>
-                <zs:numberOfRecords>1</zs:numberOfRecords>
-                <zs:records>
-                    <zs:record>
-                        <zs:recordSchema>picaxml</zs:recordSchema>
-                        <zs:recordPacking>xml</zs:recordPacking>
-                        <zs:recordData>
-                            <record xmlns="info:srw/schema/5/picaXML-v1.0">
-            */
-            Namespace nsZS = Namespace.getNamespace("zs", "http://www.loc.gov/zing/srw/");
-            e = ((List<Element>) docJdom.getRootElement().getChild("records", nsZS).getChild("record", nsZS)
-                    .getChild("recordData", nsZS).getChildren()).get(0);
-            e = (Element) e.detach();
-        } catch ( JDOMException | IOException | NullPointerException ex) {
-        	//do nothing
-        }
-        return e;
-    }
-    
-    private static Element retrievePicaXMLByPURL(String recordIdentifer) {
-        Element e = null;
-        try {
-            String sruBaseURL = MCRConfiguration.instance().getString("MCR.Editor.Pica2MODS.sru-url");
-            URL urlSRU = new URL(sruBaseURL + "&recordSchema=picaxml&query=pica.url%3Dpurl*" + recordIdentifer.replace("/", ""));
-            BufferedReader br = new BufferedReader(new InputStreamReader(urlSRU.openStream(), "UTF-8"));
-
-            Document docJdom = new SAXBuilder().build(br, "UTF-8");
-            br.close();
-
-            Namespace nsZS = Namespace.getNamespace("zs", "http://www.loc.gov/zing/srw/");
-            e = ((List<Element>) docJdom.getRootElement().getChild("records", nsZS).getChild("record", nsZS)
-                    .getChild("recordData", nsZS).getChildren()).get(0);
-            e = (Element) e.detach();
-        } catch ( JDOMException | IOException | NullPointerException ex) {
-        	//do nothing
-        }
-        
-        return e;
-    }
-    
-    private static Element retrievePicaXMLByMyCoReID(String mcrID) {
-        Element e = null;
-        try {
-            String sruBaseURL = MCRConfiguration.instance().getString("MCR.Editor.Pica2MODS.sru-url");
-            URL urlSRU = new URL(sruBaseURL + "&recordSchema=picaxml&query=pica.url%3Drosdok*resolveid" + mcrID.replace("_", ""));
-            BufferedReader br = new BufferedReader(new InputStreamReader(urlSRU.openStream(), "UTF-8"));
-
-            Document docJdom = new SAXBuilder().build(br, "UTF-8");
-            br.close();
-
-            Namespace nsZS = Namespace.getNamespace("zs", "http://www.loc.gov/zing/srw/");
-            e = ((List<Element>) docJdom.getRootElement().getChild("records", nsZS).getChild("record", nsZS)
-                    .getChild("recordData", nsZS).getChildren()).get(0);
-            e = (Element) e.detach();
-        } catch ( JDOMException | IOException | NullPointerException ex) {
-        	//do nothing
-        }
-        return e;
-    }
-    
-    private static Element retrievePicaXMLByPPN(String ppn) {
-        Element e = null;
-        try {
-            String sruBaseURL = MCRConfiguration.instance().getString("MCR.Editor.Pica2MODS.sru-url");
-            URL urlSRU = new URL(sruBaseURL + "&recordSchema=picaxml&query=pica.ppn%3D" + ppn);
-            BufferedReader br = new BufferedReader(new InputStreamReader(urlSRU.openStream(), "UTF-8"));
-
-            Document docJdom = new SAXBuilder().build(br, "UTF-8");
-            br.close();
-
-            Namespace nsZS = Namespace.getNamespace("zs", "http://www.loc.gov/zing/srw/");
-            e = ((List<Element>) docJdom.getRootElement().getChild("records", nsZS).getChild("record", nsZS)
-                    .getChild("recordData", nsZS).getChildren()).get(0);
-            e = (Element) e.detach();
-        } catch ( JDOMException | IOException | NullPointerException ex) {
-        	//do nothing
-        }
-        return e;
+    private static Element retrieveMODSByURN(String urn) {
+        String query = "pica.urn=" + urn;
+        return modsCatService.retrieveMODSFromCatalogue(query);
     }
 
+    private static Element retrieveMODSByPURL(String recordIdentifer) {
+        String query = "pica.url=purl*" + recordIdentifer.replace("/", "");
+        return modsCatService.retrieveMODSFromCatalogue(query);
+    }
+
+    private static Element retrieveMODSByMyCoReID(String mcrID) {
+        String query = "pica.url=rosdok*resolveid" + mcrID.replace("_", "");
+        return modsCatService.retrieveMODSFromCatalogue(query);
+    }
+
+    private static Element retrieveMODSByPPN(String ppn) {
+        String query = "pica.ppn=" + ppn;
+        return modsCatService.retrieveMODSFromCatalogue(query);
+    }
 }
